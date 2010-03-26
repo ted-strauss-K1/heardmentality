d6fc5f4d39aea01378f3cbcce47fe6bc84265dd5
gopi <gopi@example.com>
2010-03-26 12:33:39 +0000

    reporting changes

--- sites/all/modules/quest_lite/scripts/subtab.js
+++ sites/all/modules/quest_lite/scripts/subtab.js
@@ -24,3 +24,30 @@ function tabactive(pmID, pmPath,id)
 }
 
 
+function loadReport(pmID, pmPath,id)
+{
+	$('tab1').set('class', '');
+	$('tab2').set('class', '');
+	$('tab3').set('class', '');
+	
+	$('tab'+pmID).set('class', 'active');
+	if(pmID == 1)
+		vPath=pmPath+"qlite/percent/"+id
+	else if(pmID == 2)
+		vPath=pmPath+"qlite/graph/"+id
+	else if(pmID == 3)
+		vPath=pmPath+"qlite/map/"+id
+		
+		$('frmGoogle').src=vPath
+	/*
+var req = new Request({
+				method: 'get',
+				url: vPath,
+				data: { },
+ 				onRequest: function() {$('contents').set('html', 'Please wait...'); },
+				onComplete: function(response) {
+					$('contents').set('html', response);
+ 				}
+	}).send();
+*/
+}
