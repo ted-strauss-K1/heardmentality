6ccd2cd546a70fd033d284d8fc155eda1b0e0f8d
anna <anna@example.com>
2010-04-14 09:59:23 +0000

    * validations added for profile page (twitter and facebook fields)

---
+++ sites/all/modules/profile/js/profile.js
@@ -0,0 +1,17 @@
+function validate_profile(){
+    var facebook = $('edit-facebook').value;
+    var twitter = $('edit-twitter').value;
+    var RegExp = /^(([\w]+:)?\/\/)?(([\d\w]|%[a-fA-f\d]{2,2})+(:([\d\w]|%[a-fA-f\d]{2,2})+)?@)?([\d\w][-\d\w]{0,253}[\d\w]\.)+[\w]{2,4}(:[\d]+)?(\/([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)*(\?(&?([-+_~.\d\w]|%[a-fA-f\d]{2,2})=?)*)?(#([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)?$/;
+    if(!(RegExp.test(facebook))){
+		alert('Please enter the valid URL format for Facebook');
+        return false;
+    }
+    if(!(RegExp.test(twitter))){
+		alert('Please enter the valid URL format for Twitter');
+        return false;
+    }
+
+    return true;
+    
+}
+
