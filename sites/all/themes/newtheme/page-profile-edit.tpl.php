f1c511059f65244171138b9a434060dea6544d73
navaneetha <navaneetha@example.com>
2011-02-05 14:04:02 +0000

    theme

--- sites/all/themes/newtheme/page-profile-edit.tpl.php
+++ sites/all/themes/newtheme/page-profile-edit.tpl.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Create your Profile</title>
-<link href="css/profile-wizard.css" rel="stylesheet" type="text/css" />
-</head>
-
-</html>
 <?php
 // $Id: page.tpl.php,v 1.11.2.1 2009/04/30 00:13:31 goba Exp $
 
