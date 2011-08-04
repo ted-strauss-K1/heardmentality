76899121dfaac308acdab69f513605c55688324b
navaneetha <navaneetha@example.com>
2011-08-04 15:43:42 +0000

    front page

---
+++ sites/all/themes/hmware/page-front.tpl.php
@@ -0,0 +1,14 @@
+<?php include "header.tpl.php"; ?>
+<div class="contarea">
+    <div class="left-map">
+        <div align="center" class="head"><span>HEARD MENTALITY</span><?php print t('snappy welcome, descriptive text, enticement to add issue or vote.'); ?></div>
+        <?php print $left; ?>
+    </div>
+    
+    <div class="happening-now">
+        <div class="title"><?php print t('HAPPENING RIGHT NOW');?></div>
+        <?php print $right; ?>
+    </div>
+    <div class="clr"></div>
+</div>
+<?php include "footer.tpl.php"; ?>
