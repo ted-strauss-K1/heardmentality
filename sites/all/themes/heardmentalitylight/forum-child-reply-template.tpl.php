ba159e8e9e86e932d78773831bf5655cec99fd2c
senthilkumar <senthilkumar@example.com>
2012-02-22 07:21:26 +0000

    519:User link on name and profile picture

--- sites/all/themes/heardmentalitylight/forum-child-reply-template.tpl.php
+++ sites/all/themes/heardmentalitylight/forum-child-reply-template.tpl.php
@@ -1,6 +1,6 @@
 <?php	
 $path = '<front>';
-$sitelink = url($path, array('absolute' => TRUE)).'/';
+$sitelink = url($path, array('absolute' => TRUE)).'/'; 
 ?>
 <li id="reply-block-<?php print $items['cid'];?>">
       <a href="javascript:void(0);" class="icon flag2" onclick="open_flag_box(<?php print $items['cid'];?>, 'comment')" title="flag this Reply"></a>
