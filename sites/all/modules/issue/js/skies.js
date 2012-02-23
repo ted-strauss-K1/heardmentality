bd1e9818abdbe2ae7f6d79ec7adb9eb33034b57e
mikelsons.luc <mikelsons.luc@example.com>
2012-02-23 19:43:28 +0000

    (2) highcharts test

--- sites/all/modules/issue/js/skies.js
+++ sites/all/modules/issue/js/skies.js
@@ -12,75 +12,12 @@ Highcharts.theme = {
 		plotBackgroundColor: {
 			linearGradient: [0, 0, 250, 500],
 			stops: [
-				[0, 'rgba(255, 255, 255, 1)'],
-				[1, 'rgba(255, 255, 255, 0)']
+				[0, 'rgba(1, 1, 255, 1)'],
+				[1, 'rgba(255, 1, 1, 0)']
 			]
 		},
 		plotBorderWidth: 1,
                 shadow: true
-	},
-	title: {
-		style: { 
-			color: '#3E576F',
-			font: '16px Helvetica, sans-serif'
-		}
-	},
-	subtitle: {
-		style: { 
-			color: '#6D869F',
-			font: '12px Helvetica, sans-serif'
-		}
-	},
-	xAxis: {
-		gridLineWidth: 0,
-		lineColor: '#C0D0E0',
-		tickColor: '#C0D0E0',
-		labels: {
-			style: {
-				color: '#666',
-				rotation: 90,
-				fontWeight: 'bold'
-			}
-		},
-		title: {
-			style: {
-				color: '#666',
-				font: '12px Helvetica, sans-serif'
-			}				
-		}
-	},
-	yAxis: {
-		alternateGridColor: 'rgba(255, 255, 255, .5)',
-		lineColor: '#C0D0E0',
-		tickColor: '#C0D0E0',
-		tickWidth: 1,
-		labels: {
-			style: {
-				color: '#666',
-				fontWeight: 'bold'
-			}
-		},
-		title: {
-			style: {
-				color: '#666',
-				font: '12px Helvetica, sans-serif',
-				enabled: null
-			}				
-		}
-	},
-	legend: {
-     layout: 'horizontal',
-     align: 'right',
-     verticalAlign: 'bottom',
-     x: 0,
-     y: 0,
-     borderWidth: 0,
-     floating: true
-  },
-	labels: {
-		style: {
-			color: '#3E576F'
-		}
 	}
 };
 
