4ec6854a4f75da458336ed7c777237818cadb547
senthilkumar <senthilkumar@example.com>
2012-02-27 09:26:23 +0000

    510:Posted by - Moderated by

--- sites/all/modules/moderator/moderate-history-thread.tpl.php
+++ sites/all/modules/moderator/moderate-history-thread.tpl.php
@@ -1,6 +1,13 @@
 <?php global $base_url;
 $path = '<front>';
 $sitelink = url($path, array('absolute' => TRUE)).'/';
+/**Rallydev:510**/
+$modName = str_replace(':Owner',' ',$modName,$replace_count); 
+$operation = 'Moderated by';
+if($replace_count) {
+  $operation = 'Posted by';
+}
+/**/
 ?>
-<p class="issue-meta"><?php print t('Posted by'); ?> <a class="username" href="<?php print $sitelink.'profile/'.$modName; ?>"><?php print $modName; ?></a> <?php print t('on'); ?> <?php print $updatedOn;?>.</p>
+<p class="issue-meta"><?php print t($operation); ?> <a class="username" href="<?php print $sitelink.'profile/'.$modName; ?>"><?php print $modName; ?></a> <?php print t('on'); ?> <?php print $updatedOn;?>.</p>
 <hr>
