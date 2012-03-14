da5463640788ea036a21c97bd8940cfdc7d49133
Polina Sheveleva <polina@qliff.com>
2012-03-14 17:24:21 +0000

    Fixes scrollbar on homepage

--- sites/all/themes/heardmentalitylight/javascripts/custom_scripts.js
+++ sites/all/themes/heardmentalitylight/javascripts/custom_scripts.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 	{
 			
 			if (jQuery.browser.mobile == false) {
-				jQuery('.container .nine .grey-box .uactivity').jScrollPane({scrollbarWidth:12, scrollbarMargin:0, showArrows:true});
+				jQuery('.container .nine .grey-box .uactivity').jScrollPane({scrollbarWidth:12, scrollbarMargin:0, showArrows:false});
 			}
 
 	});
