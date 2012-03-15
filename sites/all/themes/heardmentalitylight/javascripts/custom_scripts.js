5e069ba96af3af1569d7dd3a51e19d41d023c54b
Polina Sheveleva <polina@qliff.com>
2012-03-15 08:44:23 +0000

    Fixes scrollbar on homepage on resize

--- sites/all/themes/heardmentalitylight/javascripts/custom_scripts.js
+++ sites/all/themes/heardmentalitylight/javascripts/custom_scripts.js
@@ -16,6 +16,10 @@ $(document).ready(function() {
 			if (jQuery.browser.mobile == false) {
 				jQuery('.container .nine .grey-box .uactivity').jScrollPane({scrollbarWidth:12, scrollbarMargin:0, showArrows:true});
 			}
+			
+			$('body.front .grey-box').resize(function(e) {
+				jQuery('.container .nine .grey-box .uactivity').jScrollPane({scrollbarWidth:12, scrollbarMargin:0, showArrows:true});
+			});
 
 	});
 	
