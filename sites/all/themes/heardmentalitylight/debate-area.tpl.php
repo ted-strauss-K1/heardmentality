67585cb20b51981733bd33552ba253ac73b9fe4b
Polina Sheveleva <polina@qliff.com>
2012-03-19 09:06:38 +0000

    debate section - html structure

--- sites/all/themes/heardmentalitylight/debate-area.tpl.php
+++ sites/all/themes/heardmentalitylight/debate-area.tpl.php
@@ -1,40 +1,44 @@
 <?php global $user;
 $loginBoxClass = $user->uid==0?'openlogin_box':'';
 ?>
+
+<div id="debate_work_area">
+<div class="inner">
+
 <div class="expanding">
-              <h3 class="din floatleft">></h3>
-              <h6 value="Reply" id="add-arg" class="add-comment button <?php print $loginBoxClass;?>"><?php print t('Add an Argument'); ?></h6>
+            <h6 value="Reply" id="add-arg" class="<!--add-comment button--> <?php print $loginBoxClass;?>"><?php print t('Add an Argument'); ?></h6>
 
             <ul>
               <li>
-            <div class="leave-a-comment">
-              <?php print $addNewDebateForm; ?>
-            </div>
-            </li>
-          </ul>
+				<div class="leave-a-comment">
+					<?php print $addNewDebateForm; ?>
+				</div>
+              </li>
+			</ul>
 </div>
-<hr class="short">
-<div class="expanding">
-        <h6 class="blue-link"><?php print t('FILTER ARGUMENTS'); ?></h6>
-        <ul>
-        <?php print $debateFilterForm; ?>
-        </ul>
 
-</div>
-<hr class="short">
 <div class="expanding">
-        <h6 class="blue-link" id="deb-ana"><?php print t('Debate Statistics'); ?></h6>
+        <h6 class="button"><?php print t('Show only'); ?></h6>
+        <ul>
+			<?php print $debateFilterForm; ?>
+        </ul>
+</div>
+
+<div class="expanding">
+        <h6 class="button" id="deb-ana"><?php print t('Debate statistics'); ?></h6>
         <div id="analytics-area">
           <?php print $debateStatistics; ?>
           <div id="deb-ana-load-txt"></div>
-          <br class="clear">
+
           <div id="load-deb-statics"></div>
-          <br class="clear">
         </div>
-
 </div>
 
-<hr>
-<div class="comments expanding">
-        <?php print $debateList; ?>
-</div>
+</div><!--/.inner -->
+</div><!--/#debate_work_area -->
+
+<div id="debate_list_area">
+	<div class="comments expanding">
+			<?php print $debateList; ?>
+	</div>
+</div><!--/#debate_list_area -->
