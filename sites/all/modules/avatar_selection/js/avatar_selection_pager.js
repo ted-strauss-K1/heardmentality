b3887d0d125475c00e0300cdb1268c2bf092f987
Dmitry Larionov <dmitry.larionov@qliff.com>
2012-03-21 08:15:23 +0000

    contrib modules update

--- sites/all/modules/avatar_selection/js/avatar_selection_pager.js
+++ sites/all/modules/avatar_selection/js/avatar_selection_pager.js
@@ -1,4 +1,3 @@
-/* jQueryId: avatar_selection_pager.js,v 1.1.2.3 2009/06/07 23:34:23 snpower Exp jQuery */
 
 /**
  * Fetches the new page and puts it inline.
@@ -7,26 +6,20 @@
  * @param id   - The element id whose contents will get replaced.
  * @param url - The URL for the new page.
  * @param page  - The page number to request.
- * @param js_file  - The javascript file to run upon completion.
  */
-function fetchPage(form_id, id, url, page, js_file) {
-  jQuery("body").css({'opacity': 0.5});
-  jQuery("#avatar-selection-loading").show();
-  jQuery.get(url, {page: page}, function(data, status) {
-    var selects = jQuery(data).find(id);
-    jQuery(id).html(selects);
-    var pager = jQuery(data).find(".avatar-selection-pager-nav");
-    jQuery(".avatar-selection-pager-nav").html(pager);
-    jQuery.getScript(js_file);
+function fetchPage(form_id, id, url, page) {
+  $("body").css({'opacity': 0.5});
+  $("#avatar-selection-loading").show();
+  $.get(url, {page: page}, function(data, status) {
+    var selects = $(data).find(id);
+    $(id).html(selects);
+    var pager = $(data).find(".avatar-selection-pager-nav");
+    $(".avatar-selection-pager-nav").html(pager);
+    Drupal.attachBehaviors(data);
     var action = url + "?page="+page;
-    jQuery(form_id).attr("action", action);
-    jQuery("#avatar-selection-loading").hide();
-    jQuery("body").css({'opacity': null});
-        // handle radio buttons
-    radio_button_handler();
-
-    // handle image selection
-    image_click_handler();
+    $(form_id).attr("action", action);
+    $("#avatar-selection-loading").hide();
+    $("body").css({'opacity': 1});
   });
   return false;
 }
