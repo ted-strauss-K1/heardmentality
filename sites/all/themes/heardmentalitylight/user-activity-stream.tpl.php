a1ff06ea6b8b8e21a8bedea3c2bd61d483326224
navaneetha.krishnan <navaneetha.krishnan@example.com>
2012-03-29 11:06:17 +0000

    Profile page static text changes

--- sites/all/themes/heardmentalitylight/user-activity-stream.tpl.php
+++ sites/all/themes/heardmentalitylight/user-activity-stream.tpl.php
@@ -4,7 +4,7 @@ global $base_url;
 
 <div class="top-grey-box">
   			<div class="sort-by left20">
-  				<span class="activity"><?php print t('ACTIVITY'); ?>:</span> <?php print $filters; ?>
+  				<span class="activity"><?php print t('Activity'); ?>:</span> <?php print $filters; ?>
   			</div>
 			</div>
 			<div class="grey-box">
