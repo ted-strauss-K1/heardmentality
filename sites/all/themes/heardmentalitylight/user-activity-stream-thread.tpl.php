204e62d03c7638f330ec7d32ccac9288b66a7622
navaneetha.krishnan <navaneetha.krishnan@example.com>
2012-03-29 11:03:36 +0000

    Profile page static text changes

--- sites/all/themes/heardmentalitylight/user-activity-stream-thread.tpl.php
+++ sites/all/themes/heardmentalitylight/user-activity-stream-thread.tpl.php
@@ -8,18 +8,18 @@ $sitelink = url($path, array('absolute' => TRUE)).'/';
 ?>
 <li>
         <?php if($message_id == 'add_question'){?>
-	<p class="profile action-item"><?php print t('Added a new issue'); ?>: "
+	<p class="profile action-item"><?php print t('added a new issue'); ?>: "
             <a><?php print l($data['q_title'], $url); ?>?</a>"</p>
         <?php }else if($message_id == 'add_vote'){?>
-        <p class="profile action-item"><?php print t('Voted on the issue'); ?>: "
+        <p class="profile action-item"><?php print t('voted on the issue'); ?>: "
             <a><?php print l($data['q_title'], $url); ?>?</a>"</p>
 <!--        <p class="profile action-comment-ref"><?php print t($node->body); ?></p>-->
         <?php }else if($message_id == 'add_debate'){?>
-        <p class="profile action-item"><?php print t('Posted an argument on the issue'); ?>: "
+        <p class="profile action-item"><?php print t('posted an argument on the issue'); ?>: "
             <a href="<?php print $sitelink.$url.'#forum-block-'.$data['did'];?>"><?php print t($data['q_title']); ?>?</a>"</p>
         <p class="profile action-comment-ref"><?php print t($data['d_title']); ?></p>
         <?php }else if($message_id == 'add_resource'){?>
-        <p class="profile action-item"><?php print t('Posted a reference on the issue'); ?>: "
+        <p class="profile action-item"><?php print t('posted a reference on the issue'); ?>: "
             <a href="<?php print $sitelink.$url.'#forum-block-'.$data['rid'];?>"><?php print t($data['q_title']); ?>?</a>"</p>
         <p class="profile action-comment-ref">
             <?php
@@ -38,7 +38,7 @@ $sitelink = url($path, array('absolute' => TRUE)).'/';
             <?php print $data['description']; ?>
         </p>
         <?php }else if($message_id == 'resource_reply'){?>
-        <p class="profile action-item"><?php print t('Posted an argument on a reference on the issue'); ?>: "
+        <p class="profile action-item"><?php print t('posted an argument on a reference on the issue'); ?>: "
             <?php print l($data['q_title'].'?', $url); ?>?"</p>
              <p class="profile action-comment-ref">
                 <?php
@@ -57,15 +57,15 @@ $sitelink = url($path, array('absolute' => TRUE)).'/';
                 <?php print $data['description']; ?>
             </p>
         <?php }else if($message_id == 'deb_agree'){?>
-        <p class="profile action-item"><?php print t('Strengthened an argument on the issue'); ?>: "
+        <p class="profile action-item"><?php print t('strengthened an argument on the issue'); ?>: "
             <a href="<?php print $sitelink.$url.'#forum-block-'.$data['did'];?>"><?php print t($data['q_title']); ?>?</a>"</p>
         <p class="profile action-comment-ref"><?php print t($data['d_title']); ?></p>
         <?php }else if($message_id == 'deb_disagree'){?>
-        <p class="profile action-item"><?php print t('Weakened an argument on the issue'); ?>: "
+        <p class="profile action-item"><?php print t('weakened an argument on the issue'); ?>: "
             <a href="<?php print $sitelink.$url.'#forum-block-'.$data['did'];?>"><?php print t($data['q_title']); ?>?</a>"</p>
         <p class="profile action-comment-ref"><?php print t($data['d_title']); ?></p>
         <?php }else if($message_id == 'resource_reply_agree' || $message_id == 'resource_reply_disagree'){
-            $token = $message_id == 'resource_reply_agree'?'Strengthened':'Weakened';
+            $token = $message_id == 'resource_reply_agree'?'strengthened':'weakened';
             ?>
         <p class="profile action-item"><?php print t($token.' an argument on a reference on the issue'); ?>: "
             <?php print l($data['q_title'].'?', $url); ?>?"</p>
@@ -86,14 +86,14 @@ $sitelink = url($path, array('absolute' => TRUE)).'/';
                 <?php print $data['description']; ?>
             </p>
         <?php }else if($message_id == 'add_badge'){?>
-        <p class="profile action-item"><?php print t('Earned the badge'); ?>: "
+        <p class="profile action-item"><?php print t('earned the badge'); ?>: "
             <?php print $data['badge']; ?>?"</p>
         <?php }else if($message_id == 'user_follow'){
                 $target = user_load($data['target_id']);
             ?>
 
         <p class="profile action-item">
-            <span class="now-foll-txt"><?php print t('Now following'); ?>: </span>
+            <span class="now-foll-txt"><?php print t('now following'); ?>: </span>
             <div class="foll-user-area"><a href="<?php print $sitelink.'profile/'.$target->name; ?>"><?php print $target->name; ?><br /><img alt="<?php print $target->name; ?>" src="<?php print UserPicture_small_src($data['target_id']);?>" width="44" height="44" /></a></p></div>
         <?php }?>
 </li>
