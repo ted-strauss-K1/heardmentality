41a14c8c5bc1ee7343a60dc71c1d8cfd7d25f93c
Dmitry Larionov <dmitry.larionov@qliff.com>
2012-03-29 08:39:02 +0000

    debate redesign fieldset and ago

--- sites/all/themes/heardmentalitylight/debate-parent-template.tpl.php
+++ sites/all/themes/heardmentalitylight/debate-parent-template.tpl.php
@@ -8,87 +8,90 @@ $items['title'] = str_replace("\n", "<br>", $items['title']);
 ?>
 <div id="forum-block-<?php print $items['debate_tnid']; ?>">
   <!--<?php if ($comments) { ?>
-    <h6 class="active">/ <?php print t('Argument'); ?></h6>
+        <h6 class="active">/ <?php print t('Argument'); ?></h6>
   <?php }
   else { ?>
-    <h6 class="active"><a href="#">/ <?php print t('Argument'); ?></a></h6>
+        <h6 class="active"><a href="#">/ <?php print t('Argument'); ?></a></h6>
   <?php } ?>-->
-  
-  
+
+
   <ul>
     <li>
-	
-	<div class="arg">
-		<a href="#" class="icon flag2" title="flag this argument"></a>
-		<span class="sum"><?php $diff = $items['vote_up'] - $items['vote_down'];
-  if ($diff > 0) {?>
-      <span class="positive"> <?php print $diff; ?></span>
-  <?php } 
- elseif ($diff < 0) { ?>
-     <span class="negative"> <?php print $diff; ?></span>
-<?php } 
-   else { ?>
-     <span class="null"> <?php print $diff; ?></span>
-  <?php } 
-  ?> 
-  </span>
-		<p class="argument_body"><?php print t($items['title']); ?><a href="#" class="translate"><?php print t('Translate'); ?></a></p>
-	  
-		<div class="position">
-			<?php print $strength; ?>
-		</div>
-	  
-		<div class="userinfo-debate">
-			<span class="date"><?php print t('posted '); ?><span><?php echo $items['ago']; ?></span></span>
-			<a class="user-thumb" href="<?php print $sitelink . 'profile/' . $items['uname']; ?>"><img src="<?php print UserPicture_small_src($items['uid']); ?>" alt="<?php print $items['uname']; ?>" class="user-thumb" ></a>
-			<span class="name"><a href="<?php print $sitelink . 'profile/' . $items['uname']; ?>"><?php print $items['uname']; ?></a></span>
-		</div>
-		
-		<div class="position-question">
-			<?php print $ratings; ?>
-		</div>
-		
-		<?php if ($reply_box): ?>
-        <h6 value="Reply" id="reply" class="add-comment button light <?php print $loginBoxClass; ?>"><?php print t('Reply'); ?></h6>
-        <ul>
-          <div id="reply-msg-<?php print $items['debate_tnid']; ?>" class="suc-msg"></div>
-          <div id="reply-comment"> 
-            <?php print $reply_box; ?>
-          </div>
-        </ul>
-		<?php endif; ?>
-	</div>
-	
-	<div class="replies">
-	
-    <?php if ($comments): ?>
 
-        <!--<h6 class="active">/ <?php print $reply_count; ?> <?php print t('REPLIES'); ?></h6>-->
-		
-        <ul><!--<div class="top-arrow"></div>-->
-          <div id="all_replybox_<?php print $items['debate_tnid']; ?>">
-			<?php print $comments; ?>
-          </div>
-        </ul>
-		<?php endif; ?>
-      
-		<p class="comment-meta"><?php print t('Show '); ?><?php print $reply_count; ?><?php print t(' more replies'); ?></p>
+      <div class="arg">
+        <a href="#" class="icon flag2" title="flag this argument"></a>
+        <span class="sum"><?php $diff = $items['vote_up'] - $items['vote_down'];
+  if ($diff > 0) { ?>
+            <span class="positive"> <?php print $diff; ?></span>
+          <?php }
+          elseif ($diff < 0) { ?>
+            <span class="negative"> <?php print $diff; ?></span>
+          <?php }
+          else { ?>
+            <span class="null"> <?php print $diff; ?></span>
+          <?php }
+          ?> 
+        </span>
+        <p class="argument_body"><?php print t($items['title']); ?><a href="#" class="translate"><?php print t('Translate'); ?></a></p>
 
-		<!--<?php
-			if ($delete):
-					print $delete;
-					print t('del debateparenttpl');
-			endif;
-		?>-->
+        <div class="position">
+          <?php print $strength; ?>
+        </div>
 
-	</div>
+        <div class="userinfo-debate">
+          <span class="date"><?php print t('posted '); ?><span><?php echo $items['ago']; ?></span></span>
+          <a class="user-thumb" href="<?php print $sitelink . 'profile/' . $items['uname']; ?>"><img src="<?php print UserPicture_small_src($items['uid']); ?>" alt="<?php print $items['uname']; ?>" class="user-thumb" ></a>
+          <span class="name"><a href="<?php print $sitelink . 'profile/' . $items['uname']; ?>"><?php print $items['uname']; ?></a></span>
+        </div>
+
+        <div class="position-question">
+          <?php print $ratings; ?>
+        </div>
+
+        <?php if ($reply_box): ?>
+          <h6 value="Reply" id="reply" class="add-comment button light <?php print $loginBoxClass; ?>"><?php print t('Reply'); ?></h6>
+          <ul>
+            <div id="reply-msg-<?php print $items['debate_tnid']; ?>" class="suc-msg"></div>
+            <div id="reply-comment"> 
+              <?php print $reply_box; ?>
+            </div>
+          </ul>
+        <?php endif; ?>
+      </div>
+
+      <div class="replies">
+
+        <?php if ($comments): ?>
+
+
+    <!--<h6 class="active">/ <?php print $reply_count; ?> <?php print t('REPLIES'); ?></h6>-->
+          <fieldset class ="collapsible collapsed"> 
+            <ul><!--<div class="top-arrow"></div>-->
+              <div id="all_replybox_<?php print $items['debate_tnid']; ?>">
+                <?php print $comments; ?>
+              </div>
+            </ul>
+
+          <?php endif; ?>
+
+          <legend class="comment-meta"><?php print t('Show '); ?><?php print $reply_count; ?><?php print t(' more replies'); ?></legend> 
+
+          <!--<?php
+          if ($delete):
+            print $delete;
+            print t('del debateparenttpl');
+          endif;
+          ?>-->
+          
+        </fieldset> 
+      </div>
     </li>
   </ul>
 </div>
 
 <div id="flag-arg-<?php print $items['debate_tnid']; ?>" title="<?php print t('FLAG THIS ITEM'); ?>" class="form-flag" style="display: none">
-	<input type="hidden" value="forum" id="flagtype" />
-	<?php print t('Please Wait...'); ?>
+  <input type="hidden" value="forum" id="flagtype" />
+  <?php print t('Please Wait...'); ?>
 </div>
 
 <script type="text/javascript">
--- sites/all/themes/heardmentalitylight/forum-child-reply-template.tpl.php
+++ sites/all/themes/heardmentalitylight/forum-child-reply-template.tpl.php
@@ -1,41 +1,49 @@
-<?php	
+<?php
 $path = '<front>';
-$sitelink = url($path, array('absolute' => TRUE)).'/'; 
-$items['reply_content'] = str_replace("\r\n", "<br>", $items['reply_content']);  
+$sitelink = url($path, array('absolute' => TRUE)) . '/';
+$items['reply_content'] = str_replace("\r\n", "<br>", $items['reply_content']);
 $items['reply_content'] = str_replace("\n", "<br>", $items['reply_content']);
 ?>
 
-<li id="reply-block-<?php print $items['cid'];?>">
+<li id="reply-block-<?php print $items['cid']; ?>">
   <span class=<?php print $items['class'];
-  ?>><?php print $items['sign']?></span>
-    <a href="javascript:void(0);" class="icon flag2" onclick="open_flag_box(<?php print $items['cid'];?>, 'comment')" title="flag this Reply"></a>
-	
+?>><?php print $items['sign'] ?></span>
+  <a href="javascript:void(0);" class="icon flag2" onclick="open_flag_box(<?php print $items['cid']; ?>, 'comment')" title="flag this Reply"></a>
 
-	<p class="reply_body">
-		<?php print t($items['reply_content']); ?>
-		<a href="#" class="translate"><?php print t('Translate'); ?></a><span class="userinfo-reply">&nbsp;-&nbsp;<span class="name"><a href="<?php print $sitelink . 'profile/' . $items['uname']; ?>"><?php print $items['uname']; ?></a></span>&nbsp;<span class="date"><?php echo $items['ago']; ?></span></span>
-	</p>
-	<!--<p class="comment-meta"><a><?php //print t('No Replies'); ?></a></p>-->
-  
-	<div class="position-question">
-		<?php print $ratings; ?>
-    </div>
 
-    <?php if($delete):
-        print $delete;
-        endif;
-    ?>
+  <p class="reply_body">
+    <?php print t($items['reply_content']); ?>
+    <a href="#" class="translate"><?php print t('Translate'); ?></a>
+    <span class="userinfo-reply">&nbsp;-&nbsp;
+      <span class="name">
+        <a href="<?php print $sitelink . 'profile/' . $items['uname']; print $items['ago'] . " ago" ?>">
+          <?php print $items['uname']; ?></a></span>&nbsp;
+      <span class="date"><?php echo $items['ago']; ?>
+      </span>
+    </span>
+  </p>
+  <!--<p class="comment-meta"><a><?php //print t('No Replies');  ?></a></p>-->
+
+  <div class="position-question">
+    <?php print $ratings; ?>
+  </div>
+
+  <?php
+  if ($delete):
+    print $delete;
+  endif;
+  ?>
 
 </li>
-<div id="flag-comm-<?php print $items['cid'];?>" title="<?php print t('FLAG THIS ITEM'); ?>" class="form-flag" style="display: none">
-    <input type="hidden" value="comment" id="flagtype" />
-    <?php print t('Please Wait...'); ?>
+<div id="flag-comm-<?php print $items['cid']; ?>" title="<?php print t('FLAG THIS ITEM'); ?>" class="form-flag" style="display: none">
+  <input type="hidden" value="comment" id="flagtype" />
+<?php print t('Please Wait...'); ?>
 </div>
 <script type="text/javascript">
-    $('#flag-comm-<?php print $items['cid'];?>').dialog({
-		autoOpen: false,
-		modal: false,
-		minWidth: 320,
-		resizable:false
-	});
+  $('#flag-comm-<?php print $items['cid']; ?>').dialog({
+    autoOpen: false,
+    modal: false,
+    minWidth: 320,
+    resizable:false
+  });
 </script>
