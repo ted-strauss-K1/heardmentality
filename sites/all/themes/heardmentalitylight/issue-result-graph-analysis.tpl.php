87693266d7ed06cb157df6c762481f8e6bcf84f5
Dmitry Larionov <dmitry.larionov@qliff.com>
2012-03-30 09:49:24 +0000

    vote history

--- sites/all/themes/heardmentalitylight/issue-result-graph-analysis.tpl.php
+++ sites/all/themes/heardmentalitylight/issue-result-graph-analysis.tpl.php
@@ -1,30 +1,30 @@
 <div id="results">
-<h2 class="din"><?php print t('Results'); ?></h2>
-				<div class="vote-results">
-			  	<ul>
-                                  <li class="full">
+  <h2 class="din"><?php print t('Results'); ?></h2>
+  <div class="vote-results">
+    <ul>
+      <li class="full">
 
-							<p class="result-title dinbold"><?php print t('Ethnicity'); ?></p>
-			  	    <?php print $report_percent;?>
-			  	  </li>
-                                  <li class="full">
-			  	    <p class="result-title dinbold"><?php print t('Quick facts'); ?></p>
-                                    <div id="insight-view" style="width:100%">
-                                    <?php print $insight_view;?>
-                                    </div>
-			  	  </li>
-			  	  <li class="full">
-			  	    
-                                    <p class="result-title dinbold"><?php print t('Vote History'); ?></p>
-			  	    <?php print $map;?>
+        <p class="result-title dinbold"><?php print t('Ethnicity'); ?></p>
+        <?php print $report_percent; ?>
+      </li>
+      <li class="full">
+        <p class="result-title dinbold"><?php print t('Quick facts'); ?></p>
+        <div id="insight-view" style="width:100%">
+          <?php print $insight_view; ?>
+        </div>
+      </li>
+      <li class="full">
 
-			  	  </li>
-			  	  <li class="full">
-							<p class="result-title dinbold"><?php print t('Vote History'); ?></p>
-			  	    <?php print $report_graph;?>
-			  	  </li>
-			  	  
-		  	    <br class="clear">
-					</ul>
-				</div>
+        <p class="result-title dinbold"><?php print t('Vote History'); ?></p>
+        <?php print $map; ?>
+
+      </li>
+      <li class="full">
+        <p class="result-title dinbold"><?php print t('Vote History'); ?></p>
+        <?php print $report_graph; ?>
+      </li>
+
+      <br class="clear">
+    </ul>
+  </div>
 </div>
