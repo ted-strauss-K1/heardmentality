3b1027f2dae46c860cc6ff8df706b442184f69ce
Polina Sheveleva <polina@qliff.com>
2012-04-03 10:12:49 +0000

    Arguments - change reply structure

--- sites/all/themes/heardmentalitylight/debate-parent-template.tpl.php
+++ sites/all/themes/heardmentalitylight/debate-parent-template.tpl.php
@@ -57,7 +57,7 @@ $items['title'] = str_replace("\n", "<br>", $items['title']);
 		
         <fieldset class="collapsible collapsed reply_wrapper">
 		
-		<legend class="comment-meta"><a href="#"><?php print t('&#9658; '); ?><?php print $reply_count; ?><?php print t(' replies'); ?></a></legend>
+		<legend class="comment-meta"><?php print t('&#9658; '); ?><?php print $reply_count; ?><?php print t(' replies'); ?></legend>
             <div class="fieldset-wrapper">
 				<ul>
 				  <div id="all_replybox_<?php print $items['debate_tnid']; ?>">
