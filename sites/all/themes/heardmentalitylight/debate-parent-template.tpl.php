45d4aa1a3ae01442ffbbcdc41ccf44be19adb42b
Polina Sheveleva <polina@qliff.com>
2012-04-05 10:20:30 +0000

    Arguments

--- sites/all/themes/heardmentalitylight/debate-parent-template.tpl.php
+++ sites/all/themes/heardmentalitylight/debate-parent-template.tpl.php
@@ -61,7 +61,7 @@ $items['title'] = str_replace("\n", "<br>", $items['title']);
 		
         <fieldset class="collapsible collapsed reply_wrapper">
 		
-		<legend class="comment-meta"><?php print t('&#9658; '); ?><?php print $reply_count; ?><?php print t(' replies'); ?></legend>
+		<legend class="comment-meta"><?php print t('&#9658; '); ?><span><?php print $reply_count; ?><?php print t(' replies'); ?></span></legend>
             <div class="fieldset-wrapper">
 				<ul>
 				  <div id="all_replybox_<?php print $items['debate_tnid']; ?>">
