85c9d8b9fbd608bf00990a77f5faa2f33543d88e
Polina Sheveleva <polina@qliff.com>
2012-04-11 17:19:46 +0000

    vote result

--- sites/all/themes/heardmentalitylight/issue-result-graph-analysis.tpl.php
+++ sites/all/themes/heardmentalitylight/issue-result-graph-analysis.tpl.php
@@ -2,29 +2,46 @@
   <h2 class="din"><?php print t('Results'); ?></h2>
   <div class="vote-results">
     <ul>
-      <li class="full">
+      <li class="part">
 
-        <p class="result-title dinbold"><?php print t('Ethnicity'); ?></p>
-        <?php print $report_percent; ?>
+        <p class="result-title dinbold"><?php print t('by'); ?></p>
+        <div class="inner filter"><?php print $report_percent; ?></div>
+		
       </li>
-      <li class="full">
+      <li class="part">
+	  
         <p class="result-title dinbold"><?php print t('Quick facts'); ?></p>
-        <div id="insight-view" style="width:100%">
+        <div id="inner insight-view">
           <?php print $insight_view; ?>
         </div>
+		
       </li>
-      <li class="full">
+	  <li class="full">
+	  
+		<p class="result-title dinbold"><?php print t('Legend'); ?></p>
+		<div class="inner legend">
+			<dl>
+				<dd>Name variant 1</dd><dt>color</dt>
+				<dd>Name variant 2</dd><dt>color</dt>
+				<dd>Name variant 3</dd><dt>color</dt>
+				<dd>Name 4</dd><dt>color</dt>
+				<dd>Name variant 5</dd><dt>color</dt>
+			</dl>
+		</div>
+		
+	  </li>      
+	  <li class="part">
+	  
+        <p class="result-title dinbold"><?php print t('Vote history'); ?></p>
+        <div class="inner graf"><?php print $report_graph; ?></div>
+		
+      </li>
+      <li class="part">
 
-        <p class="result-title dinbold"><?php print t('Vote History'); ?></p>
-        <?php print $map; ?>
+        <p class="result-title dinbold"><?php print t('Vote map'); ?></p>
+        <div class="inner map"><?php print $map; ?></div>
 
       </li>
-      <li class="full">
-        <p class="result-title dinbold"><?php print t('Vote History'); ?></p>
-        <?php print $report_graph; ?>
-      </li>
-
-      <br class="clear">
     </ul>
   </div>
 </div>
