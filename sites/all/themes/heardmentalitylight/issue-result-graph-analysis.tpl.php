cea6055706fc1aff9b2c32e0738a060db0fbd78d
Polina Sheveleva <polina@qliff.com>
2012-04-12 08:17:26 +0000

    vote result

--- sites/all/themes/heardmentalitylight/issue-result-graph-analysis.tpl.php
+++ sites/all/themes/heardmentalitylight/issue-result-graph-analysis.tpl.php
@@ -2,21 +2,21 @@
   <h2 class="din"><?php print t('Results'); ?></h2>
   <div class="vote-results">
     <ul>
-      <li class="part">
+      <li class="part"><div class="space">
 
         <p class="result-title dinbold"><?php print t('by'); ?></p>
         <div class="inner filter"><?php print $report_percent; ?></div>
 		
-      </li>
-      <li class="part margin">
+      </div></li>
+      <li class="part margin"><div class="space">
 	  
         <p class="result-title dinbold"><?php print t('Quick facts'); ?></p>
         <div id="insight-view" class="inner">
           <?php print $insight_view; ?>
         </div>
 		
-      </li>
-	  <li class="full">
+      </div></li>
+	  <li class="full"><div class="space">
 	  
 		<p class="result-title dinbold"><?php print t('Legend'); ?></p>
 		<div class="inner legend">
@@ -29,19 +29,19 @@
 			</dl>
 		</div>
 		
-	  </li>      
-	  <li class="part">
+	  </div></li>      
+	  <li class="part"><div class="space">
 	  
         <p class="result-title dinbold"><?php print t('Vote history'); ?></p>
         <div class="inner graf"><?php print $report_graph; ?></div>
 		
-      </li>
-      <li class="part margin">
+      </div></li>
+      <li class="part margin"><div class="space">
 
         <p class="result-title dinbold"><?php print t('Vote map'); ?></p>
         <div class="inner map"><?php print $map; ?></div>
 
-      </li>
+      </div></li>
     </ul>
   </div>
 </div>
