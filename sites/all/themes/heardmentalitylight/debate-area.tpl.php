40f13da738daa7ee68a6b384d9ac3dbda8962f44
Dmitry Larionov <dmitry.larionov@qliff.com>
2012-04-23 11:20:12 +0000

    debate redesign

--- sites/all/themes/heardmentalitylight/debate-area.tpl.php
+++ sites/all/themes/heardmentalitylight/debate-area.tpl.php
@@ -57,8 +57,8 @@ $loginBoxClass = $user->uid == 0 ? 'openlogin_box' : '';
 
     <ul>
       <li><a href="#tabs-1">older</a></li>
-      <li><a href="<?php print $base_path ?>/heardmentality/issue/<?php print $nid ?>/tab_content/1/1">recent</a></li>
-      <li><a href="<?php print $base_path ?>/issue/<?php print $nid ?>/tab_content/1/2">supported</a></li>
+      <li><a href="<?php print $base_path ?>/issue/<?php print $nid; ?>/tab_content/1/1">recent</a></li>
+      <li><a href="<?php print $base_path ?>/issue/<?php print $nid; ?>/tab_content/1/2">supported</a></li>
     </ul>
   </h2>
   <div id="empty_helper">&nbsp;</div>
