7429417abfeb14d53902bd2427372978cbc9c419
Dmitry Larionov <dmitry.larionov@qliff.com>
2012-04-23 11:18:44 +0000

    debate redesign

--- sites/all/modules/issue/js/add_new_form.js
+++ sites/all/modules/issue/js/add_new_form.js
@@ -27,7 +27,7 @@ jQuery('#add-new-debate-form').live('submit', function(e){
       flag = parseInt(flag)+1;
     }
   }
- /* if(title.length < 2){
+  /* if(title.length < 2){
     jQuery('#deb-err').html('<span>Please let us know what you think.</span>');
     return false;
   }
@@ -35,38 +35,46 @@ jQuery('#add-new-debate-form').live('submit', function(e){
     jQuery('#deb-err').html('<span>You must choose at least one suppose or oppose.</span>');
     return false;
   }*/
- // else{
-    jQuery('#add_new_debate').hide();
-    jQuery('#sub_loader').show();
-    var data=jQuery(this).serialize();
-    jQuery.ajax({
-      type: 'POST',
-      dataType: 'json',
-      url: jQuery(this).attr('action'),
-      data: data,
-      success: function(msg){
-        jQuery('#add_debate_wrapper').slideUp('slow');
-        //jQuery('#add-new-debate-form').clearForm();
-        jQuery('#add-new-debate-form select').val(0);
-        jQuery('#add-new-debate-form textarea').val('');
-        $('#debate_list_area').tabs("load", 0);
-        $('#debate_list_area').tabs("load", 2);
-        jQuery('#deb-err').fadeIn("slow");
-        jQuery('#deb-err').html(msg.message); 
-        if(msg.success_post == 1) {
-          jQuery('#deb-err').css('color','green'); 
-        }
-
-      //  jQuery(msg.content).prependTo('.comments');
-      },
-      complete: function(){
-        jQuery('#sub_loader').hide();
-        jQuery('#add_new_debate').show();
-        $('#leave_comment_area').delay(5000).slideUp(400).removeClass('visible_ar').addClass('hidden_ar');
-        $('#deb-err').delay(3000).slideUp(400);
-        $('#add-arg').removeClass('expanded');
+  // else{
+  jQuery('#add_new_debate').hide();
+  jQuery('#sub_loader').show();
+  var data=jQuery(this).serialize();
+  jQuery.ajax({
+    type: 'POST',
+    dataType: 'json',
+    url: jQuery(this).attr('action'),
+    data: data,
+    success: function(msg){
+      jQuery('#add_debate_wrapper').slideUp('slow');
+      //jQuery('#add-new-debate-form').clearForm();
+      jQuery('#add-new-debate-form select').val(0);
+      jQuery('#add-new-debate-form textarea').val('');
+      $('#debate_list_area').tabs("load", 0);
+      $('#debate_list_area').tabs("load", 2);
+      jQuery('#deb-err').fadeIn("slow");
+      jQuery('#deb-err').html(msg.message); 
+      if(msg.success_post == 1) {
+        jQuery('#deb-err').css('color','green'); 
       }
-    });
- // }
+
+    //  jQuery(msg.content).prependTo('.comments');
+    },
+    complete: function(){
+      jQuery('#sub_loader').hide();
+      jQuery('#add_new_debate').show();
+      $('#leave_comment_area').delay(5000).slideUp(400).removeClass('visible_ar').addClass('hidden_ar');
+      $('#deb-err').delay(3000).slideUp(400);
+      $('#add-arg').removeClass('expanded');
+    }
+  });
+  // }
   return false;
+});
+
+
+$(document).ready(function() {
+  $('#inc_ref').change(function(){
+    $('.resources').toggle();
+  });
+
 });
--- sites/all/themes/heardmentalitylight/debate-area.tpl.php
+++ sites/all/themes/heardmentalitylight/debate-area.tpl.php
@@ -43,7 +43,7 @@ $loginBoxClass = $user->uid == 0 ? 'openlogin_box' : '';
     print ' Arguments'; ?></span>
 
     <div class="show_only">
-      <span class="button" href="">&#9660; Show only</span> <div class="inc"><form id="inc_ref"><input type="checkbox" value="include references" id="inc_check" /><label for="inc_check">Include References</label></form></div>
+      <span class="button" href="">&#9660; Show only</span> <div class="inc"><form id="inc_ref"><input type="checkbox" checked="yes" value="include references" id="inc_check" /><label for="inc_check">Include References</label></form></div>
       <div class="popup hidden">
         <span class="title">Show all</span>
         <?php foreach ($show_only as $key => $answer) {
