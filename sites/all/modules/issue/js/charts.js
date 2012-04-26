d4124c444d9bfccf6a559930775ee0c5c7bc20c7
Dmitry Larionov <dmitry.larionov@qliff.com>
2012-04-26 10:41:27 +0000

    highstock

--- sites/all/modules/issue/js/charts.js
+++ sites/all/modules/issue/js/charts.js
@@ -1,6 +1,6 @@
 $(function() {
- // $('#results').click(function(){
-    /*    $.getJSON('/issue/highstock/ajax/2242', function(data) {
+  // $('#results').click(function(){
+  /*    $.getJSON('/issue/highstock/ajax/2242', function(data) {
         // Create the chart
         window.chart = new Highcharts.StockChart({
             chart : {
@@ -25,69 +25,58 @@ $(function() {
         });
     });*/
   
-    var count = $('#tot_ans').val();
-    var values = [];
-    for (var i = 0; i < count; i++) {
-      values[i] = i;
-    }
-    var seriesOptions = [],
-    yAxisOptions = [],
-    seriesCounter = 0,
+  var count = $('#tot_ans').val();
+  var values = [];
+  for (var i = 0; i < count; i++) {
+    values[i] = i;
+  }
+  var seriesOptions = [],
+  yAxisOptions = [],
+  seriesCounter = 0,
   
-    colors = Highcharts.getOptions().colors;
-    $.each(values, function(i, value) {
+  colors = Highcharts.getOptions().colors;
+  $.each(values, function(i, value) {
 
-      $.getJSON('/issue/highstock/ajax/2242/'+ value, function(data) {
+    $.getJSON('/issue/highstock/ajax/2242/'+ value, function(data) {
 
-        seriesOptions[i] = {
-          name: data.name,
-          data: data.data
-        };
-        // As we're loading the data asynchronously, we don't know what order it will arrive. So
-        // we keep a counter and create the chart when all the data is loaded.
-        seriesCounter++;
+      seriesOptions[i] = {
+        name: data.name,
+        data: data.data
+      };
+      // As we're loading the data asynchronously, we don't know what order it will arrive. So
+      // we keep a counter and create the chart when all the data is loaded.
+      seriesCounter++;
 
-        if (seriesCounter == count) {
-          createChart();
-        }
-      });
+      if (seriesCounter == count) {
+        createChart();
+      }
     });
-    function createChart() {
-console.log(seriesOptions);
-      chart = new Highcharts.StockChart({
-        chart: {
-          renderTo: 'container_graph'
-        },
+  });
+  function createChart() {
+    console.log(seriesOptions);
+    chart = new Highcharts.StockChart({
+      chart: {
+        renderTo: 'container_graph'
+      },
 
-        rangeSelector: {
-          selected: 1
-        },
-
-        yAxis: {
-          labels: {
-            formatter: function() {
-              return (this.value > 0 ? '+' : '') + this.value + '%';
-            }
-          },
-          plotLines: [{
-            value: 0,
-            width: 2,
-            color: 'silver'
-          }]
-        },
+      rangeSelector: {
+        selected: 1
+      },
+           
+      plotOptions: {
+        series: {
+          compare: 'value',
+          pointStart: Date.UTC(2012, 3, 1),
+          pointInterval: 3600 * 1000 * 24
+        }
+      },
             
-        plotOptions: {
-          series: {
-            compare: 'value'
-          }
-        },
-            
-        tooltip: {
-          pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
-          valueDecimals: 0
-        },
-        series: seriesOptions
-      });
-    }
- // });
+      /*    tooltip: {
+        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+        valueDecimals: 0 
+      }, */
+      series: seriesOptions
+    });
+  }
+// });
 });
