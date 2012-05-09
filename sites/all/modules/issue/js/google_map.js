f06112355f38334eefdeb60a8e0e8c879ccfb09e
Dmitry Larionov <dmitry.larionov@qliff.com>
2012-05-09 19:19:27 +0000

    debate redesign

--- sites/all/modules/issue/js/google_map.js
+++ sites/all/modules/issue/js/google_map.js
@@ -1,23 +1,29 @@
 $(document).ready(function() {
   var map;
   function initialize() {
-    var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
-    var myOptions = {
-      zoom: 0,
-      center: myLatlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    map = new google.maps.Map(document.getElementById("map_div"), myOptions);
-    var nid = $('#curr_nid').val();
-    var beaches = [];
     $.getJSON('/issue/gmap/ajax/'+ nid, function(data) {
+      var beaches = [];
+      var latitude, longitude;
       $.each(data, function(key, val) {
         for (i in val) {
           //  beaches.push(val[i].latitude, val[i].longitude);
           beaches[i]= [val[i].latitude, val[i].longitude];
         }
+        latitude = val[0].latitude;
+        longitude = val[0].longitude;
       });
     
+      
+  
+      var myLatlng = new google.maps.LatLng(latitude, longitude);
+      var myOptions = {
+        zoom: 0,
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+      map = new google.maps.Map(document.getElementById("map_div"), myOptions);
+      var nid = $('#curr_nid').val();
+      
       setMarkers(map, beaches);
     });
   }
@@ -26,9 +32,15 @@ $(document).ready(function() {
     for (var i = 0; i < locations.length; i++) {
       var beach = locations[i];
       var myLatLng = new google.maps.LatLng(beach[0], beach[1]);
-      var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map
+      var marker = new google.maps.Circle({
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FF0000",
+        fillOpacity: 0.35,
+        map: map,
+        center: myLatLng,
+        radius: 10000
       });
     }
   }
