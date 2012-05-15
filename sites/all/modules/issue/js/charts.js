48c8142d44885b3be2d5baf49160317b18fd6bd8
Dmitry Larionov <dmitry.larionov@qliff.com>
2012-05-15 17:22:41 +0000

    debate redesign

--- sites/all/modules/issue/js/charts.js
+++ sites/all/modules/issue/js/charts.js
@@ -334,84 +334,12 @@ function load_chart(arg) {
 
 $(function() {
   var arg = $('#dochg').val();
- // load_chart(arg);
+  load_chart(arg);
   $('#dochg').live("change", function(){
-  //  load_chart($(this).val());
+    load_chart($(this).val());
   //return false;
   });
   
 });
 
 
-
-$(function () {
-    var chart;
-    $(document).ready(function() {
-        chart = new Highcharts.Chart({
-            chart: {
-                renderTo: 'chart_div',
-                type: 'bar'
-            },
-            title: {
-                text: 'Historic World Population by Region'
-            },
-            subtitle: {
-                text: 'Source: Wikipedia.org'
-            },
-            xAxis: {
-                categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
-                title: {
-                    text: null
-                }
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'Population (millions)',
-                    align: 'high'
-                }
-
-            },
-            tooltip: {
-                formatter: function() {
-                    return ''+
-                        this.series.name +': '+ this.y +' millions';
-                }
-            },
-            plotOptions: {
-                bar: {
-                    dataLabels: {
-                        enabled: true
-                    }
-                }
-            },
-
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'top',
-                x: -100,
-                y: 100,
-                floating: true,
-                borderWidth: 1,
-                backgroundColor: '#FFFFFF',
-                shadow: true
-            },
-
-            credits: {
-                enabled: false
-            },
-
-            series: [{
-                name: 'Year 1800',
-                data: [107, 31, 635, 203, 2]
-            }, {
-                name: 'Year 1900',
-                data: [133, 156, 947, 408, 6]
-            }, {
-                name: 'Year 2008',
-                data: [973, 914, 4054, 732, 34]
-            }]
-        });
-    });
-});
