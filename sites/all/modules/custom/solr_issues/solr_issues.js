b5267da09c9bf3a7e1779fc75e06d6ba9a78c5fe
Dmitry Larionov <dmitry.larionov@qliff.com>
2012-06-15 21:39:25 +0000

    solr search

---
+++ sites/all/modules/custom/solr_issues/solr_issues.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+  $('#search-solr-block').click(function(){
+    $.post(Drupal.settings.base_url + '/issues_solr2/ajax/' + 'post',
+    {
+      js: 1
+    }, 
+    function(data){
+      console.log(data);
+    //  $('#linkbox').html(data.data.regions.apachesolr_ajax);
+    $('#linkbox').html(data.data);
+    },
+      'json'
+      );
+  
+    return false;
+  })
+  Drupal.apachesolr_ajax = {};
+  Drupal.apachesolr_ajax.response_callback = function (data) {
+    for (var setting in data.settings) {
+      Drupal.settings[setting] = data.settings[setting];
+    }
+
+    var list = [];
+
+    // Schedule items for removal to reduce page jumpiness.
+    if (blocks) {
+      for (var block in blocks) {
+        list.push($(blocks[block]));
+      }
+    }
+    for (var region in data.regions) {
+      if (region == 'apachesolr_ajax') {
+        if (content) {
+          var elements = $(data.regions[region]);
+          Drupal.attachBehaviors(elements.appendTo($(content).empty()));
+        }
+      }
+      else {
+        for (var block in data.regions[region]) {
+          if (regions[region] && blocks[block]) {
+            var elements = $(data.regions[region][block]);
+            Drupal.attachBehaviors(elements.appendTo(regions[region]));
+          }
+        }
+      }
+    }
+    for (var i = 0, l = list.length; i < l; i++) {
+      list[i].remove();
+    }
+  };
+});
