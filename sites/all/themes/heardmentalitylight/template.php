df4136d38a5a7e497373e3b403fe5038341954b6
Dmitry Larionov <dmitry.larionov@qliff.com>
2012-06-18 11:37:51 +0000

    debate redesign

--- sites/all/themes/heardmentalitylight/template.php
+++ sites/all/themes/heardmentalitylight/template.php
@@ -107,15 +107,15 @@ if($vars['node']->type == 'static_pages' || $vars['node']->type == 'page'|| $var
 /*Rally dev 481 - jQuery problem*/
     
 
-    $js = drupal_add_js(NULL, NULL, 'header'); //get header js files in an array
+ //   $js = drupal_add_js(NULL, NULL, 'header'); //get header js files in an array
    
-    unset($js['core']['misc/jquery.js']); //unset default drupal jquery js
-    $js['core'] = array_reverse($js['core'], 1); //make our own jquery file first (see note)
+   // unset($js['core']['misc/jquery.js']); //unset default drupal jquery js
+  //  $js['core'] = array_reverse($js['core'], 1); //make our own jquery file first (see note)
 
-    if($vars['template_files'][1]=='page-issue-create'){
-    unset($js['theme']['sites/all/themes/heardmentalitylight/javascripts/jquery-1.5.1.min.js']); //unset default drupal jquery js
+ //   if($vars['template_files'][1]=='page-issue-create'){
+ //   unset($js['theme']['sites/all/themes/heardmentalitylight/javascripts/jquery-1.5.1.min.js']); //unset default drupal jquery js
     //echo '<pre>';    print_r($js);exit;
-    }
+ //   }
     
     /*
       if ($vars['content'] && $vars['node']->type != 'forum') {
@@ -123,7 +123,7 @@ if($vars['node']->type == 'static_pages' || $vars['node']->type == 'page'|| $var
       }
 
      */
- $vars['scripts'] = drupal_get_js('header', $js); //create script tags and set them to $scripts
+// $vars['scripts'] = drupal_get_js('header', $js); //create script tags and set them to $scripts
    
 }
 
