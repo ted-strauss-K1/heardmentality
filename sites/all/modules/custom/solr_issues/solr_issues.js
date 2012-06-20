d38f00c35c8f6ae462e74965ecfe6b86c10c2362
Dmitry Larionov <dmitry.larionov@qliff.com>
2012-06-20 18:28:55 +0000

    issues search

--- sites/all/modules/custom/solr_issues/solr_issues.js
+++ sites/all/modules/custom/solr_issues/solr_issues.js
@@ -1,9 +1,35 @@
 $(document).ready(function(){
   $('#search-solr-block').click(function(){
     var text = $('#edit-search-text').val();
+    var tid = [];
+    var categ = $('#edit-category-subject').val();
+    var defarea = $('#edit-category-defarea').val();
+    var defdetail = $('#edit-category-defdetail').val();
+    
+    
+    
+    
+    // tid = $('#edit-category-subject').val() + $('#edit-category-defarea').val() + $('#edit-category-defdetail').val();
+    if (categ != null) {
+      categ.map(function(index, element) {
+        tid.push(index);
+      });
+      if (defarea != null) {
+        defarea.map(function(index, element) {
+          tid.push(index);
+        });
+        if (defdetail != null) {
+          defdetail.map(function(index, element) {
+            tid.push(index);
+          });
+        }
+      }
+    }
+
     $.post(Drupal.settings.base_url + '/issues_solr2/ajax/' + text,
     {
-      js: 1
+      js: 1,
+      tid: tid
     }, 
     function(data){
       //  $('#linkbox').html(data.data.regions.apachesolr_ajax);
