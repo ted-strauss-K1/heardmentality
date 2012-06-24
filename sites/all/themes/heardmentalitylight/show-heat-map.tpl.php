e49c8ff2808884bca4d5728a3b5200f2cb971c08
Polina Sheveleva <polina@qliff.com>
2012-06-24 14:51:45 +0000

    homepage

--- sites/all/themes/heardmentalitylight/show-heat-map.tpl.php
+++ sites/all/themes/heardmentalitylight/show-heat-map.tpl.php
@@ -11,5 +11,5 @@ global $base_url;
         <li><span class="count"><?php print number_format($users); ?></span><span class="count-title"><a href="<?php print $base_url; ?>/google/map/users"><?php print t('Users'); ?></a></span></li>
         <li><span class="count"><?php print number_format($issues); ?></span><span class="count-title"><a href="<?php print $base_url; ?>/google/map/questions"><?php print t('Issues'); ?></a></span></li>
         <li><span class="count"><?php print number_format($votes); ?></span><span class="count-title"><a href="<?php print $base_url; ?>/google/map/answer"><?php print t('Votes'); ?></a></span></li>
-        <li><span class="count"><?php print number_format($online); ?></span><span class="count-title"><a href="<?php print $base_url; ?>/google/map/online"><?php print t('Members'); ?></a></span></li>
+        <li><span class="count"><?php print number_format($online); ?></span><span class="count-title"><a href="<?php print $base_url; ?>/google/map/online"><?php print t('Online'); ?></a></span></li>
 </ul>
