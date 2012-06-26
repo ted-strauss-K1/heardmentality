b323d42c36924ae819d6f539e1847921a7c3b441
Dmitry Larionov <dmitry.larionov@qliff.com>
2012-06-26 16:12:01 +0000

    default index.php

--- index.php
+++ index.php
@@ -10,12 +10,9 @@
  * All Drupal code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt.
  */
-print('Hello');exit;
+
 require_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
 
 $return = menu_execute_active_handler();
 
