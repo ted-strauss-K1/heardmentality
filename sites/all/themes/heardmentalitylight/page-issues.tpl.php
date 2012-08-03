d1fbc2ba00da3e2d75dd8c1da1b3e0eb25b2acba
Dmitry Larionov <dmitry.larionov@qliff.com>
2012-08-03 09:01:26 +0000

    issues link path

--- sites/all/themes/heardmentalitylight/page-issues.tpl.php
+++ sites/all/themes/heardmentalitylight/page-issues.tpl.php
@@ -9,124 +9,90 @@ $directoryPath = $base_path . $directory;
 <!--[if IE 9 ]><html class="ie9" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>"> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!--><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>"> <!--<![endif]-->
   <head>
-
-    <!-- Basic Page Needs
-================================================== -->
     <?php print $head; ?>
-    <meta charset="utf-8" />
-    <title><?php print $head_title; ?></title>
-    <meta name="description" content="Heard Mentality">
-    <meta name="author" content="">
-    <!--[if lt IE 9]>
-            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+	<meta charset="utf-8" />
+	<title><?php print $head_title; ?></title>
+	<meta name="description" content="Heard Mentality">
+	<meta name="author" content="">
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
 
-    <!-- Mobile Specific Metas
-================================================== -->
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
-    <!-- CSS
-================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <?php print $styles; ?>
-    <!-- JS
-    ================================================== -->
-    <?php print $scripts; ?>
-    <!-- Favicons
-    ================================================== -->
-    <link rel="shortcut icon" href="<?php print $directoryPath; ?>/images/favicon.ico">
-    <link rel="apple-touch-icon" href="<?php print $directoryPath; ?>/images/apple-touch-icon.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php print $directoryPath; ?>/images/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php print $directoryPath; ?>/images/apple-touch-icon-114x114.png" />
+	<?php print $scripts; ?>
+	<link rel="shortcut icon" href="<?php print $directoryPath;?>/images/favicon.ico">
+	<link rel="apple-touch-icon" href="<?php print $directoryPath;?>/images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php print $directoryPath;?>/images/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php print $directoryPath;?>/images/apple-touch-icon-114x114.png" />
 
-    <!-- socialize.js script should only be included once -->
-    <script type="text/javascript">
-      var conf=
-        {
-        enabledProviders: 'facebook,twitter,yahoo,messenger,google,linkedin,myspace,aol,foursquare,orkut,vkontakte,renren'
-      }
-    </script>
+  <!-- socialize.js script should only be included once -->
+  <script type="text/javascript">
+  var conf=
+  {
+  	enabledProviders: 'facebook,twitter,yahoo,messenger,google,linkedin,myspace,aol,foursquare,orkut,vkontakte,renren'
+  }
+  </script>
   </head>
-  <body class="<?php print $classes; ?>">
+  <body class="<?php print $classes; ?>issues_search_page">
 
-    <!-- Primary Page Layout
-    ================================================== -->
+	<div class="header">
+		<?php include 'header.tpl.php';?>
+	</div>
 
-    <div class="header">
-      <?php include 'header.tpl.php'; ?>
-      <div class="container">
+    <div class="container">
+	
+		<?php if ($tabs): ?>
+			<div class="tabs"><?php print $tabs; ?></div>
+		<?php endif; ?>
+		<?php if($messages):?>
+			<div class="toggler profile-message">
+        
+	    	<div id="effect" class="">
 
-        <div class="toggler profile-message" style="height:auto;width:auto">
-          <div class="clear"></div>
-
-          <?php if ($messages): ?>
-            <div id="effect" class="" style="width:auto;height:auto">
-              <div class="message top-message">
-                <p class="double" style="disply:block">
-                  <?php print $messages; ?>
-                </p>
-              </div>
-              <a href="#" id="button" class="hide-message"><span class="ui-icon ui-icon-closethick">Hide</span></a>
-            </div>
-          <?php endif; ?>
-          <br class="clear">
-
-        </div>
-
-      </div>
-
-      <div class="container">
+						<div class="message top-message">
+							<p class="double" style="disply:block">
+                                 <?php print $messages; ?>
+                            </p>
+						</div>
+                        <a href="#" id="button" class="hide-message"><span class="ui-icon ui-icon-closethick"><?php print t('Hide'); ?></span></a>
+	    	</div>
+			</div>
+		<?php endif; ?>
 
         <div class="twelve columns">
+		
+		  <h2 class="dinbold page-title"><?php print t('Issues'); ?></h2>
 
-
-          <?php if ($issue_search_area): print $issue_search_area;
-          endif; ?>
-
-            <?php if ($issue_filter_area): ?>
-            <div class="sort-white-box">
-            <?php print $issue_filter_area; ?>
-            </div>
-            <?php endif; ?>
-          <div class="white-box">
-<?php print $content; ?>
+          <div class="white-box issues_search">
+				<?php print $content; ?>
           </div>
         </div>
+		
         <div class="four columns">
 		
-          <div class="filter_search">
+          <div class="filter_search grey-box poll-box issues_search">
 		  
 			<?php print $right_search; ?>
 		  
 		  </div>
 		  
-          <div class="toggler profile-message">
-            <div id="effect">
-
-<?php print $profile_complete_alert; ?>
-
-            </div>
-          </div>
-          <div class="grey-box">
-            <div class="icon cat text"></div>
-            <ul class="tags">
-				<?php if ($popular_categories): print $popular_categories;
-				endif; ?>
-            </ul>
-            <br class="clear">
-            <div class="clear"></div>
-            <br>
-          </div>
+          <?php if($issue_real_info):?>
+			<div class="grey-box poll-box issues_search">
+			
+                <?php print $issue_real_info; ?>
+			 
+			</div>
+          <?php endif; ?>
+		  
         </div>	
 
-      </div>
+    </div>
 
       <!-- container -->
 <?php include 'footer.tpl.php'; ?>
 
-
-
-      <!-- End Document
-      ================================================== -->
   </body>
 </html>
 <script type="text/javascript">
