ec592a49a3edc4fd9d890c8d8e66e93830e105b7
Polina Sheveleva <polina@qliff.com>
2012-08-16 13:23:12 +0000

    profile

--- sites/all/themes/heardmentalitylight/user-profile-bio.tpl.php
+++ sites/all/themes/heardmentalitylight/user-profile-bio.tpl.php
@@ -13,20 +13,20 @@
                         <?php print $settings['bio']; ?>
                 </p>
                  <?php if($settings['mywebsite']): ?>
-                <span class="URL"><?php print t('Web'); ?>: <a href="<?php print $settings['mywebsite']; ?>" target="_blank"><?php print $settings['mywebsite']; ?></a></span>,
+                <span class="URL block"><strong><?php print t('Web'); ?>:</strong> <a href="<?php print $settings['mywebsite']; ?>" target="_blank"><?php print $settings['mywebsite']; ?></a></span>
                 <?php endif; ?>
                 <?php if($settings['email']): ?>
-                <span class="URL"><?php print t('Email'); ?>: <?php print $settings['email']; ?></span>,
+                <span class="URL block"><strong><?php print t('Email'); ?>:</strong> <?php print $settings['email']; ?></span>,
                 <?php endif; ?>
                 <?php if($settings['twitter']): ?>
-                <span class="URL"><?php print t('Twitter'); ?>: <a href="<?php print $settings['twitter']; ?>" target="_blank"><?php print $settings['twitter']; ?></a></span>,
+                <span class="URL block"><strong><?php print t('Twitter'); ?>:</strong> <a href="<?php print $settings['twitter']; ?>" target="_blank"><?php print $settings['twitter']; ?></a></span>,
                 <?php endif; ?>
                 <?php if($settings['facebook']): ?>
-                <span class="URL"><?php print t('Facebook'); ?>: <a href="<?php print $settings['facebook']; ?>" target="_blank"><?php print $settings['facebook']; ?></a></span>
+                <span class="URL block"><strong><?php print t('Facebook'); ?>:</strong> <a href="<?php print $settings['facebook']; ?>" target="_blank"><?php print $settings['facebook']; ?></a></span>
                 <?php endif; ?>
                 <?php if($settings['other_links']):
                     foreach($settings['other_links'] as $name => $url) {?>
-                    <span class="URL"><?php print ucwords(t($name)); ?>: <a href="<?php print $url; ?>" target="_blank"><?php print $url; ?></a></span>
+                    <span class="URL block"><strong><?php print ucwords(t($name)); ?>:</strong> <a href="<?php print $url; ?>" target="_blank"><?php print $url; ?></a></span>
                     <?php }?>
                 <?php endif; ?>
         </div>
