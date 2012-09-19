da9cb29ae502d85a40da1e5054331b720b16a9c1
skullhole <skullhole@gmail.com>
2012-09-19 15:21:51 +0700

    Follow page template

---
+++ sites/all/themes/heardmentalitylight/user-profile-follow.tpl.php
@@ -0,0 +1,11 @@
+<?php global $user; ?>
+<div class="dark-grey-box ">
+        <div class="following">
+                <label for="following" class="following"><?php print t('You\'re Following'); ?></label>
+                xxx
+                <br class="clear">
+                <hr class="dark">
+
+        </div>
+        <br class="clear">
+</div>
