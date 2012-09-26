4edb0d8d209b6d04e312c8f9ad8140e611936791
skullhole <skullhole@gmail.com>
2012-09-26 13:29:22 +0700

    Issue view preprocess performance improvements

--- sites/all/themes/heardmentalitylight/node-poll.tpl.php
+++ sites/all/themes/heardmentalitylight/node-poll.tpl.php
@@ -1,55 +1,41 @@
 <div id="new_vote">
+  <!-- Part 1 -->
+  <div class="part part1">
+    <h2 class="din"><?php print t(rtrim($title, "?")); ?>?</h2>
+    <div class="qd">
+      <p class="description"><?php print theme('issue_description', $context); ?></p>
+    </div>
+    <div class="poll-vote-area">
+      <?php if($allowvotes == '' ) : ?>
+      <div class="vote-count-poll">
+        <?php foreach($indVoteCounts as $chorder => $vcount) : ?>
+          <div class="post-vote-result"><span class="vote-count dinbold"><?php print $vcount; ?></span><br /><span class="vote-count-title din">votes</span></div>
+        <?php endforeach; ?>
+      </div>
+      <?php endif; ?>
+      <div class="voting-pane"><?php print drupal_get_form('issue_vote_form', $node) #$content;?></div>
+    </div>
+  </div><!-- /div.part1 -->
 
-<!-- Part 1 -->
+  <!-- Part 2 -->
+  <div class="part part2">
+    <div id="shareDiv" class="clearfix"></div>
+  </div><!-- /div.part2 -->
 
-<div class="part part1">
-
-<h2 class="din"><?php print t(rtrim($title, "?")); ?>?</h2>
-
-<div class="qd">
-<p class="description">
-  <?php print theme('issue_description', $context); ?>
-
-
-</p>
-</div>
-
-<!--<div class="issue-things"></div>-->
-<div class="poll-vote-area">
-
-<?php if($allowvotes == '' ): ?>
-   <div class="vote-count-poll">
-       <?php foreach($indVoteCounts as $chorder => $vcount){?>
-       <div class="post-vote-result"><span class="vote-count dinbold"><?php print $vcount; ?></span><br /><span class="vote-count-title din">votes</span></div>
-       <?php }?>
-   </div>
-<?php endif; ?>
-<div class="voting-pane"><?php print $content;?></div>
-</div>
-</div><!-- /div.part1 -->
-
-<!-- Part 2 -->
-
-<div class="part part2">
-
-<div id="shareDiv" class="clearfix"></div>
-
-</div><!-- /div.part2 -->
-
-
-<?php
-$nodepath = 'node/'.$nid;
-$pagePath = url($nodepath, array('absolute' => TRUE)).'/'; ?>
-<script type="text/javascript">
+  <?php
+    $nodepath = 'node/'.$nid;
+    $pagePath = url($nodepath, array('absolute' => TRUE)).'/';
+  ?>
+  <script type="text/javascript">
     var act = new gigya.services.socialize.UserAction();
     act.setUserMessage("Your comment here...");
     act.setTitle("<?php print t($title); ?>");
     act.setDescription("<?php
-                        $context = str_replace(array("\r", "\n"), '', $context);
-                     print t(htmlentities($context));?>");
+      $context = str_replace(array("\r", "\n"), '', $context);
+      print t(htmlentities($context));
+    ?>");
     act.setLinkBack("<?php print $pagePath; ?>");
     act.addActionLink("<?php print t($title); ?>","<?php print $pagePath; ?>");
-
     var showShareBarUI_params=
     {
       containerID: 'shareDiv',
@@ -57,15 +43,14 @@ $pagePath = url($nodepath, array('absolute' => TRUE)).'/'; ?>
       userAction: act,
       layout:'horizontal'
     }
-</script>
+  </script>
 
-<script type="text/javascript">
-   gigya.services.socialize.showShareBarUI(conf,showShareBarUI_params);
-   function showDesc(){
+  <script type="text/javascript">
+    gigya.services.socialize.showShareBarUI(conf,showShareBarUI_params);
+    function showDesc(){
        jQuery('#sp-desc').css('height', 'auto');
        jQuery('#more-desc').hide();
-   }
-</script>
-
+    }
+  </script>
 </div>
         
