2b11c60bf40fa158a8535094c59f6696eb026b1b
skullhole <skullhole@gmail.com>
2012-09-27 23:20:52 +0700

    phpinfo

--- what.php
+++ what.php
@@ -1,8 +1,3 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: skullhole
- * Date: 9/27/12
- * Time: 11:17 PM
- * To change this template use File | Settings | File Templates.
- */
+
+phpinfo();
