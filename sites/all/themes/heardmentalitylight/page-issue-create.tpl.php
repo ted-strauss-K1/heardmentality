d605e02239ef34f80c2c8a9a08fa28beb7077cab
Polina Sheveleva <polina@qliff.com>
2012-10-07 00:26:09 +0700

    edit profile / create issues

--- sites/all/themes/heardmentalitylight/page-issue-create.tpl.php
+++ sites/all/themes/heardmentalitylight/page-issue-create.tpl.php
@@ -1,4 +1,7 @@
-<?php $directoryPath = $base_path . $directory; ?>
+<?php
+global $user, $gSitePath, $apikey;
+$directoryPath = $base_path . $directory;
+?>
 <!doctype html>
 <!--[if lt IE 7 ]><html class="ie ie6" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>"> <![endif]-->
 <!--[if IE 7 ]><html class="ie ie7" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>"> <![endif]-->
@@ -6,7 +9,6 @@
 <!--[if IE 9 ]><html class="ie9" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>"> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!--><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>"> <!--<![endif]-->
 <head>
-
   <?php print $head; ?>
 	<meta charset="utf-8" />
 	<title><?php print $head_title; ?></title>
@@ -17,9 +19,7 @@
 	<![endif]-->
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
-
   <?php print $styles; ?>
-
 
 	<link rel="shortcut icon" href="<?php print $directoryPath;?>/images/favicon.ico">
 	<link rel="apple-touch-icon" href="<?php print $directoryPath;?>/images/apple-touch-icon.png">
@@ -28,53 +28,47 @@
 
   <!-- socialize.js script should only be included once -->
   <script type="text/javascript">
-  var conf=
-  {
-  	enabledProviders: 'facebook,twitter,yahoo,messenger,google,linkedin,myspace,aol,foursquare,orkut,vkontakte,renren'
-  }
+    var conf={	enabledProviders: 'facebook,twitter,yahoo,messenger,google,linkedin,myspace,aol,foursquare,orkut,vkontakte,renren' }
   </script>
+
 </head>
-<body class="<?php print $classes; ?>">
+<body class="<?php print $classes; ?>profile_view_page">
 
 	<div class="header">
 		<?php include 'header.tpl.php';?>
-	<div class="container">
-
-    <div class="toggler profile-message" style="height:auto;width:auto">
-      <div class="clear"></div>
-    </div>
-
-  </div>
+	</div>
 
 	<div class="container">
-    <div class="eleven columns">
-		  <h2 class="dinbold page-title"><?php print t('New Issue'); ?></h2>
-			<?php print $content;?>
+	
+		<?php if ($tabs): ?>
+				<div class="tabs"><?php print $tabs; ?></div>
+		<?php endif; ?>
+		
+		<div class="eleven columns">
+			  <h2 class="dinbold page-title"><?php print t('New Issue'); ?></h2>
+			  <?php print $content;?>
 		</div>
 
 		<div class="five columns">
+				<div class="grey-box top clearfix">
+				<!--<div class="icon guides text"></div>-->
+				<label for="guidelines" class="tags-on"><?php print t('Guidelines'); ?></label><br>
+					<p class="issue-meta"><?php print t('Please follow Heard Mentality\'s'); ?><a class="bolded"> <?php print t('Guidelines and Principles'); ?></a> <?php print t('when creating a new Issue.
+					All issues should be:');?> <ul class="issue-meta"><li><?php print t('concise'); ?></li> <li><?php print t('clean'); ?></li> <li><?php print t('original'); ?></li><li><?php print t('unbiased'); ?></li></ul></p>
 
-      <br>
-			<div class="grey-box">
-		  	<div class="icon guides text"></div>
-		    <label for="" class="tags-on"><?php print t('Guidelines'); ?></label><br>
-				<p class="issue-meta"><?php print t('Please follow Heard Mentality\'s'); ?><a class="bolded"> <?php print t('Guidelines and Principles'); ?></a> <?php print t('when creating a new Issue.
-				All issues should be:');?> <ul class="issue-meta"><li><?php print t('concise'); ?></li> <li><?php print t('clean'); ?></li> <li><?php print t('original'); ?></li><li><?php print t('unbiased'); ?></li></ul></p>
+				  <hr class="short">
+				  <br>
+				  <p class="issue-meta"><?php print t('When you submit Issues that are unique, clear, popular and require no editing, you can'); ?> <a class="bolded"><?php print t('win a badge.');?></a></p>
 
-			  <hr class="short">
-			  <br>
-			  <p class="issue-meta"><?php print t('When you submit Issues that are unique, clear, popular and require no editing, you can'); ?> <a class="bolded"><?php print t('win a badge.');?></a></p>
-
-			  <div class="clear"></div>
-			</div>
+				  <div class="clear"></div>
+				</div>
 
 		</div>
 
-	</div><!-- container -->
+	</div>
 
-  <br class="clear"><br /><br />
-
-  <?php include 'footer.tpl.php';?>
+  <?php include 'footer.tpl.php'; ?>
   <?php print $scripts; ?>
+
 </body>
 </html>
