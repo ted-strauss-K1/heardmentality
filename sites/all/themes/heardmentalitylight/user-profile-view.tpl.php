46b28ed5cc8ed327f99f42380dd7f0ad927b5e36
skullhole <skullhole@gmail.com>
2012-10-12 14:58:42 +0700

    Permission issue - autologin to viewed user

--- sites/all/themes/heardmentalitylight/user-profile-view.tpl.php
+++ sites/all/themes/heardmentalitylight/user-profile-view.tpl.php
@@ -1,5 +1,5 @@
-<?php print theme('follower_button', $user); ?>
-<h2 class="din"><?php print $user->name; ?></h2>
+<?php print theme('follower_button', $account); ?>
+<h2 class="din"><?php print $account->name; ?></h2>
 
 <div class="user-info">
   <p><?php print t($rank); ?></p>
@@ -8,28 +8,27 @@
 
     <?php $fields = profile_privacy_get_fields(); ?>
     <?php foreach ($fields as $field) : ?>
-      <?php if (!empty($user->{$field->name}) && profile_privacy_get_field_access($user, $field)) : ?>
+      <?php if (!empty($account->{$field->name}) && profile_privacy_get_field_access($account, $field)) : ?>
         <span class="URL block">
           <strong><?php print t($field->title); ?>:</strong>
           <?php switch ($field->name) :
             case 'profile_twitter' : ?>
-              <a href="https://twitter.com/<?php print htmlspecialchars($user->{$field->name}); ?>" target="_blank"><?php print $user->{$field->name}; ?></a>
+              <a href="https://twitter.com/<?php print htmlspecialchars($account->{$field->name}); ?>" target="_blank"><?php print $account->{$field->name}; ?></a>
             <?php break;
             case 'profile_facebook': ?>
-              <a href="http://www.facebook.com/<?php print htmlspecialchars($user->{$field->name}); ?>" target="_blank"><?php print $user->{$field->name}; ?></a>
+              <a href="http://www.facebook.com/<?php print htmlspecialchars($account->{$field->name}); ?>" target="_blank"><?php print $account->{$field->name}; ?></a>
             <?php break;
             case 'profile_website': ?>
-              <a href="<?php print htmlspecialchars($user->{$field->name}); ?>" target="_blank"><?php print $user->{$field->name}; ?></a>
+              <a href="<?php print htmlspecialchars($account->{$field->name}); ?>" target="_blank"><?php print $account->{$field->name}; ?></a>
             <?php break;
 
             default : ?>
-              <?php print $user->{$field->name}; ?>
+              <?php print $account->{$field->name}; ?>
             <?php break; ?>
           <?php endswitch; ?>
         </span>
       <?php endif; ?>
     <?php endforeach; ?>
   <?php endif; ?>
-  <?php #var_dump($user); ?>
 </div>
 
