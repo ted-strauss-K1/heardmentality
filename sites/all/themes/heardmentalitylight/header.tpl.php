ed73bf688cc8ef9b5671340192f693110378dd3d
Polina Sheveleva <polina@qliff.com>
2012-10-18 01:24:05 +0700

    design card

--- sites/all/themes/heardmentalitylight/header.tpl.php
+++ sites/all/themes/heardmentalitylight/header.tpl.php
@@ -18,19 +18,19 @@
 		<div class="navigation">
 			<div class="top-left user-menu">
 				<ul class="menu">
-          <li><?php print l(t('Issues'), 'issues', array('attributes' => array('class'=>'issues'))); ?></li>
-          <li><?php print l(t('Analysis'), $_GET['q'], array('attributes' => array('class'=>'analysis'))); ?></li>
+				  <li><?php print l(t('Issues'), 'issues', array('attributes' => array('class'=>'issues'))); ?></li>
+				  <li><?php print l(t('Analysis'), $_GET['q'], array('attributes' => array('class'=>'analysis'))); ?></li>
 				</ul>
 				<ul class="menu second">
-          <li><?php print l(t('Add an Issue'), 'issue/create', array('attributes' => array('class'=>'add'))); ?></li>
-          <?php if (user_is_logged_in()) : ?>
-            <li><?php print l(t('Following'), 'following', array('attributes' => array('class'=>'following'))); ?></li>
-          <?php endif; ?>
-          <?php
-          // TODO
-          if ( false ) : ?>
-            <li><?php print l(t('Moderator'), 'issues/list/all', array('attributes' => array('class'=>'moderator add'))); ?></li>
-          <?php endif; ?>
+				  <li><?php print l(t('Add an Issue'), 'issue/create', array('attributes' => array('class'=>'add'))); ?></li>
+				  <?php if (user_is_logged_in()) : ?>
+					<li><?php print l(t('Following'), 'following', array('attributes' => array('class'=>'following'))); ?></li>
+				  <?php endif; ?>
+				  <?php
+				  // TODO
+				  if ( false ) : ?>
+					<li><?php print l(t('Moderator'), 'issues/list/all', array('attributes' => array('class'=>'moderator add'))); ?></li>
+				  <?php endif; ?>
 				</ul>
 			</div>
 			<div class="top-right blue">
