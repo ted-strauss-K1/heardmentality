351714c49e274022ad76b563966eeaba6989d8c2
skullhole <skullhole@gmail.com>
2012-11-29 14:21:55 +0700

    Userpoints Extension update

--- sites/all/modules/custom/up/up.events.js
+++ sites/all/modules/custom/up/up.events.js
@@ -1,24 +1,22 @@
-/*
+/**
  * Function to invoke user shared userpoints
  */
 function up_share() {
-    var url = Drupal.settings.base_url + '/up_share/' + Drupal.settings.node.nid;
-    jQuery.ajax({
-        type: "POST",
-        dataType: 'json',
-        url: url,
-        data: {},
-        success: function(response){
-            //
-        }
-    });
+  var url = Drupal.settings.base_url + '/up_share/' + Drupal.settings.node.nid;
+  $.ajax({
+    type      : "POST",
+    dataType  : 'json',
+    url       : url,
+    data      : {},
+    success   : function (response) {
+      //
+    }
+  });
 }
 
-/*
+/**
  * Attach events for the share buttons
- *
- * TODO -- fix selectors
  */
-jQuery("#shareDiv-reaction0, #shareDiv-reaction1, #shareDiv-reaction2").click(function(){
-    up_share();
+$("#shareDiv-reaction0, #shareDiv-reaction1, #shareDiv-reaction2").click(function () {
+  up_share();
 });
