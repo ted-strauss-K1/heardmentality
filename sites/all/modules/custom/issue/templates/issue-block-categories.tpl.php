954599bfe5963191fc589d60d830c836865921b5
unknown <skullhole@gmail.com>
2013-01-28 17:41:43 +0700

    Fixes to hide warnings + moderation: hide buttons

--- sites/all/modules/custom/issue/templates/issue-block-categories.tpl.php
+++ sites/all/modules/custom/issue/templates/issue-block-categories.tpl.php
@@ -3,7 +3,7 @@
   <div class="icon cat text"></div>
   <ul class="tags">
     <label for="" class="tags-on"><?php print t('ACTIVE CATEGORIES'); ?></label>
-    <?php foreach ($categories as $hierarchy => $name) : ?>
+    <?php if ($categories) foreach ($categories as $hierarchy => $name) : ?>
       <li><?php print l($name, 'issues', array('query' => array('tids' => $hierarchy))); ?></li>
     <?php endforeach; ?>
   </ul>
