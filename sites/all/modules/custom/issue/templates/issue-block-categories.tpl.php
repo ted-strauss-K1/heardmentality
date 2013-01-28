355e9923e5ad3885ace22ed0a11b095b216aa495
unknown <skullhole@gmail.com>
2013-01-28 16:35:00 +0700

    Active categories block

--- sites/all/modules/custom/issue/templates/issue-block-categories.tpl.php
+++ sites/all/modules/custom/issue/templates/issue-block-categories.tpl.php
@@ -10,13 +10,16 @@
   <br class="clear">
   <hr class="short">
   <br>
-  <p class="issue-meta"><?php print t('Posted on'); ?>
-    <?php print date($node->created, 'M j, Y @ H:i'); ?>
-    <br>
+  <p class="issue-meta"><?php print t('Posted'); ?>
     <?php print t('by');?>
-    <?php print l($node->name, 'user/profile/view/' . $node->uid, array('attributes' => array('class' => 'username'))); ?>.
-    <?php if ($node->language != $original_language) : ?>
-      <?php print t('Read original post in');?> <?php print l('', sprintf('/%s/%s', $original_language, $original_path)); ?>
+    <?php print l($node->name, 'user/profile/view/' . $node->uid, array('attributes' => array('class' => 'username'))); ?>
+    <?php print t('on') ?>
+    <br>
+    <?php print date('M j, Y @ H:i', $node->created); ?>.
+    <?php if ($node->language != $original_langcode) : ?>
+      <br>
+      <?php print t('Read original post in');?>
+      <?php printf("<a href='%s'>%s</a>", sprintf('/%s/%s', $original_langcode, $original_path), t($original_language)); ?>
     <?php endif; ?>
   </p>
   <div class="clear"></div>
