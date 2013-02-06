e809be0e07b8267e6cd3dbfc2b218403e766c968
unknown <skullhole@gmail.com>
2013-02-06 13:24:54 +0700

    Add language to ajax voting

--- sites/all/modules/custom/issue/issue.view.js
+++ sites/all/modules/custom/issue/issue.view.js
@@ -4,7 +4,7 @@ $('.issue-vote-form').live('submit', function (e) {
   $.ajax({
     type:'POST',
     dataType:'json',
-    url:'/issue/ajax',
+    url:'/'+Drupal.settings.language+'/issue/ajax',
     data:form.serialize(),
     success:function (response) {
       if (!response.status) {
