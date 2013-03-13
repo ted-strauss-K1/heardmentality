beb34dc8584bc516ef9f2dcc41f0d756edfe472c
unknown <skullhole@gmail.com>
2013-03-13 09:35:45 +0700

    Google Analytics

--- sites/all/themes/heardmentalitylight/page_dn.tpl.php
+++ sites/all/themes/heardmentalitylight/page_dn.tpl.php
@@ -2,7 +2,19 @@
 <?php print $scripts; ?>
 
 <!-- GA -->
-<!-- todo paste the code here -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-26789011-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <!-- END GA -->
 
 </body>
