1d8cd74b2aa102f135963162c53a8a50cf51b1e2
unknown <skullhole@gmail.com>
2013-03-25 15:15:29 +0700

    Ajax subscriptions

---
+++ sites/all/modules/custom/issue/issue.categories.js
@@ -0,0 +1,31 @@
+$('input[type="checkbox"].issue_category').live('click', function (e) {
+  e.preventDefault();
+
+  var $$ = $(this);
+  $.ajax({
+    type        : 'POST',
+    dataType    : 'json',
+    url         : 'issue/subscribe',
+    data        : { hierarchy : $(this).attr('name') },
+    success     : function (response) {
+      if (!response.status) {
+        $.hrd.noty({
+          type  : 'error',
+          text  : response.message
+        });
+        return false;
+      }
+      //
+      $.hrd.noty({
+        type  : 'success',
+        text  : response.message
+      });
+      // checking
+      if (response.result) {
+        $$.attr('checked', 'checked');
+      } else {
+        $$.removeAttr('checked');
+      }
+    }
+  });
+});
--- sites/all/modules/custom/issue/templates/issue-block-categories.tpl.php
+++ sites/all/modules/custom/issue/templates/issue-block-categories.tpl.php
@@ -2,9 +2,9 @@
 <div class="grey-box poll-box">
   <div class="icon cat text"></div>
   <ul class="tags">
-    <label for="" class="tags-on"><?php print t('This issue is listed under'); ?></label>
+    <label class="tags-on"><?php print t('This issue is listed under'); ?></label>
     <?php if ($categories) foreach ($categories as $hierarchy => $name) : ?>
-    <li><?php print l($name, 'issues', array('query' => array('tids' => $hierarchy))); ?></li>
+    <li><?php print theme('issue_category', $name, $hierarchy); ?></li>
     <?php endforeach; ?>
   </ul>
   <br class="clear">
