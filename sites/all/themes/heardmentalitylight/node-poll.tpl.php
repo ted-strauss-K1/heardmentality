ce1b1f24d4e5d9dd7b1aa7bb5e6c6066b45b00f6
Oleg Dyachenko <skullhole@gmail.com>
2013-05-07 15:40:49 +0700

    Share This code. Module is removed

--- sites/all/themes/heardmentalitylight/node-poll.tpl.php
+++ sites/all/themes/heardmentalitylight/node-poll.tpl.php
@@ -32,8 +32,27 @@
   <?php if($page) : ?>
   <!-- Part 2 -->
   <div class="part part2">
-    <div id="shareDiv" class="clearfix"></div>
+    <div id="shareDiv" class="clearfix">
+      <span class='st_facebook_hcount' displayText='Facebook'></span>
+      <span class='st_twitter_hcount' displayText='Tweet'></span>
+      <span class='st_googleplus_hcount' displayText='Google +'></span>
+      <span class='st_linkedin_hcount' displayText='LinkedIn'></span>
+      <span class='st_sharethis_hcount' displayText='ShareThis'></span>
+      <script type="text/javascript">var switchTo5x=false;</script>
+      <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+      <script type="text/javascript">
+        stLight.options({
+          publisher: "1ace1433-6c6c-45c5-bb28-d33af72b78b4",
+          doNotHash: false,
+          doNotCopy: false,
+          hashAddressBar: false
+        });
+      </script>
+    </div>
   </div><!-- /div.part2 -->
+
+
+
 
   <script type="text/javascript">
     function showDesc(){
