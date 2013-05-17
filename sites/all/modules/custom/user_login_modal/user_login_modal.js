0b152f38b886ca747bd2d0669170f3192034f1f4
skullhole <skullhole@gmail.com>
2013-05-17 14:54:51 +0700

    Mobile switch with JS

--- sites/all/modules/custom/user_login_modal/user_login_modal.js
+++ sites/all/modules/custom/user_login_modal/user_login_modal.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
-  $(".inline-dialog").colorbox({inline:true, width:'100%', height:'100%', top: '0px', reposition: false});
+  $(".inline-dialog").colorbox($.browser.mobile ?
+  {inline:true, width:'100%', height:'100%', top: '0px', reposition: false} : {inline:true}
+  );
 
   $('#dialog_link, .openlogin_box').live('click', function () {
 
