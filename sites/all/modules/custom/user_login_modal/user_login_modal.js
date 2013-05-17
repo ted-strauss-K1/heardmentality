b8cc5b8fb6f215baa6d1251051b5ac951a0bc3a8
skullhole <skullhole@gmail.com>
2013-05-17 15:06:57 +0700

    Fix alignment issues

--- sites/all/modules/custom/user_login_modal/user_login_modal.js
+++ sites/all/modules/custom/user_login_modal/user_login_modal.js
@@ -1,17 +1,12 @@
 $(document).ready(function () {
 
-  $(".inline-dialog").colorbox($.browser.mobile ?
-  {inline:true, width:'100%', height:'100%', top: '0px', reposition: false} : {inline:true}
-  );
+  var options = $.browser.mobile ?  {inline:true, width:'100%', height:'100%', top: '0px', reposition: false} : {inline:true};
+  console.log(options);
+  $(".inline-dialog").colorbox(options);
 
   $('#dialog_link, .openlogin_box').live('click', function () {
 
     $(".inline-dialog").click();
-
-//    $('#dialog')
-//      .tabs()
-//      .dialog('open')
-//      .find('input[name="name"]').focus();
 
     return false;
   });
