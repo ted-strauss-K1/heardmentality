9aa987836a5ac2877869ecbcc336be3c96c6396e
skullhole <skullhole@gmail.com>
2013-06-03 15:57:16 +0700

    No console.log

--- sites/all/modules/custom/user_login_modal/user_login_modal.js
+++ sites/all/modules/custom/user_login_modal/user_login_modal.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
   var options = $.browser.mobile ?  {inline:true, width:'100%', height:'100%', top: '0px', reposition: false} : {inline:true};
-  console.log(options);
+
   $(".inline-dialog").colorbox(options);
 
   $('#dialog_link, .openlogin_box').live('click', function () {
