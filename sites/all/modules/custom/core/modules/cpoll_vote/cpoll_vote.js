1212355f83d37b2a5c30247effd4211385826bb9
skullhole <skullhole@gmail.com>
2013-07-01 23:29:54 +0700

    Ajax voting

---
+++ sites/all/modules/custom/core/modules/cpoll_vote/cpoll_vote.js
@@ -0,0 +1,191 @@
+$('#suggest').live('click', function (e) {
+  e.preventDefault();
+
+  var input = $('input[name="suggest[suggest_answer]"]');
+  var form = input.parents('.issue-vote-form');
+
+  // activated form
+  if (form.hasClass('active')) {
+    $.hrd.noty({
+      type:'error',
+      text:'Multiple submissions are not allowed. Wait for few seconds to change your vote' // todo translate
+    });
+    return;
+  }
+
+  // empty
+  if (!input.val()) {
+    return;
+  }
+
+  // hide form
+  input.parents('fieldset').slideUp(500);
+
+  // update not-voted status
+  form.removeClass('not-voted');
+  $.hrd.noty({
+    type:'success',
+    text: 'Your suggestion was added!' // todo translate
+  });
+
+  // update the text
+  var wrapper = $('.vote-result-empty');
+  var label = wrapper.parent().find('.ch');
+  var choice = wrapper.parent().find('input');
+  wrapper.addClass('vote-result-new').removeClass('vote-result-empty');
+  label.html(input.val());
+
+  // check radios
+  var vote = form.find('[name=vote]');
+  if (-1 != vote.val()) {
+    vote_ux(form, vote.val(), -1);
+  }
+  vote_ux_set(form, -1, 1);
+  form.find('input[name=choices][value=-1]').prop('checked', true);
+
+  // activate form
+  form.addClass('active');
+  $.ajax({
+    type:'POST',
+    dataType:'json',
+    url:/*'/'+Drupal.settings.language+*/'/cpoll_vote/ajax',
+    data: {
+      "nid"         : form.find('input[name=nid]').val(),
+      "suggestion"  : input.val()
+    },
+    success:function (response) {
+      if (!response.status) {
+        $.hrd.noty({
+          type:'error',
+          text:response.message
+        });
+
+        // show form
+        input.parents('fieldset').slideDown(500);
+        // rollback suggestion
+        wrapper.addClass('vote-result-empty').removeClass('vote-result-new');
+        label.html('');
+        // values
+        if (-1 != vote.val()) {
+          vote_ux(form, vote.val(), 1);
+        }
+        vote_ux(form, -1, -1);
+
+        form.removeClass('active');
+
+        return;
+      }
+
+      //
+      $('input[name=choices][value=-1]').val(response.chid);
+      vote.val(response.chid);
+
+      // update the form with server's data
+      var results = response.result;
+      for (var i in results) {
+        vote_ux_set(form, i, results[i]['votes']);
+      }
+      form.removeClass('active');
+
+      //
+      $.trigger('cpoll_update');
+    }
+  });
+});
+
+$('#vote').live('click', function (e) {
+  e.preventDefault();
+
+  var form = $(this).parents('.issue-vote-form');
+
+  // activated form
+  if (form.hasClass('active')) {
+    $.hrd.noty({
+      type:'error',
+      text:'Multiple submissions are not allowed. Wait for few seconds to change your vote' // todo translate
+    });
+    return;
+  }
+
+  // validate selection
+  var chid = form.find(':checked');
+  var vote = form.find('[name=vote]');
+  if (!chid || (chid.val() == vote.val())) {
+    return;
+  }
+
+  // counters
+  vote_ux(form, vote.val(), -1);
+  vote_ux(form, chid.val(),  1);
+
+  // update not-voted status
+  form.removeClass('not-voted');
+  $.hrd.noty({
+    type:'success',
+    text: 'Your vote is accepted!' // todo translate
+  });
+
+  // activate form
+  form.addClass('active');
+  $.ajax({
+    type:'POST',
+    dataType:'json',
+    url:/*'/'+Drupal.settings.language+*/'/cpoll_vote/ajax',
+    data: {
+      "nid"         : form.find('input[name=nid]').val(),
+      "chid"        : chid.val()
+    },
+    success:function (response) {
+      if (!response.status) {
+        $.hrd.noty({
+          type:'error',
+          text:response.message
+        });
+
+        // show form
+        vote_ux(form, vote.val(),  1);
+        vote_ux(form, chid.val(), -1);
+
+        form.removeClass('active');
+
+        return;
+      }
+
+      //
+      vote.val(response.chid);
+
+      // update the form with server's data
+      var results = response.result;
+      for (var i in results) {
+        vote_ux_set(form, i, results[i]['votes']);
+      }
+      form.removeClass('active');
+
+      //
+      $.trigger('cpoll_update');
+    }
+  });
+});
+
+/**
+ *
+ * @param form
+ * @param value
+ * @param delta
+ */
+function vote_ux(form, value, delta) {
+  var radio = form.find('input[name=choices][value='+value+']');
+  radio.prop('checked', -1 != delta);
+  var count = radio.parents('.radio_wrapper').find('.teaser_count_vote span');
+  count.html(delta + parseInt(count.html()));
+}
+
+/**
+ *
+ * @param form
+ * @param value
+ * @param delta
+ */
+function vote_ux_set(form, value, votes) {
+  form.find('input[name=choices][value='+value+']').parents('.radio_wrapper').find('.teaser_count_vote span').html(votes);
+}
---
+++ sites/all/themes/heardmentalitylight/node-issue.tpl.php
@@ -0,0 +1,70 @@
+<div
+  class="<?php if($teaser) : ?>search_item
+    <?php if (isset($node->taxonomy)) foreach ($node->taxonomy as $tid => $term) print ' tft-'.$tid; ?>
+    <?php if (isset($node->locations)) foreach ($node->locations as $location) print ' tfl-'.$location['name']; ?>
+  <?php endif; ?> issue-vote-wrapper"
+  id="<?php if($page) : ?>new_vote<?php else : ?><?php endif; ?>">
+  <!-- Part 1 -->
+  <?php $translation = module_exists('poll_translate') ? poll_translate_translation($node) : array(); ?>
+  <div class="part part1 <?php if($teaser) : ?>search_list<?php endif; ?>">
+    <h2 class="din<?php if($teaser) : ?> half<?php endif; ?>"><?php print
+      l( t(rtrim(!empty($translation['title'][0]) ? $translation['title'][0] : $title, "?")), $node->path, array('html' => true));
+    ?>?</h2>
+    <?php if($page) : ?>
+    <div class="qd">
+      <?php if (true || $node->moderate) : // todo ?>
+        <img src="/sites/all/themes/heardmentalitylight/images/icons/59-flag@2x.png" height="14px">
+        <?php print theme('flagger_btn_flag', $nid, 'node'); // todo ?>
+        <?php print theme('flagger_btn_flags', $nid, 'node'); ?>
+        <?php print theme('flagger_btn_history', $nid, 'node'); ?>
+        <?php if (module_exists('moderation') && moderation_check_perm()) : ?>
+          <?php print l(t('moderation'), 'moderation/issue/'.$node->nid,
+            array('attributes' => array(
+              'class' => 'moderation',
+              'original-title' => t('moderation'),
+            ))
+          ); ?>
+        <?php endif; ?>
+      <?php endif; ?>
+      <p class="description"><?php print $node->body; ?></p>
+    </div>
+    <?php endif; ?>
+    <div class="poll-vote-area">
+      <div class="voting-pane"><?php print drupal_get_form('cpoll_vote_form', $node); ?></div>
+    </div>
+  </div><!-- /div.part1 -->
+
+  <?php if($page) : ?>
+    <div class="part part2">
+      <div id="shareDiv" class="clearfix">
+        <span class='st_facebook_hcount' displayText='Facebook'></span>
+        <span class='st_twitter_hcount' displayText='Tweet'></span>
+        <span class='st_googleplus_hcount' displayText='Google +'></span>
+        <span class='st_linkedin_hcount' displayText='LinkedIn'></span>
+        <span class='st_sharethis_hcount' displayText='ShareThis'></span>
+        <script type="text/javascript">var switchTo5x=false;</script>
+        <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+        <script type="text/javascript">
+          stLight.options({
+            publisher: "1ace1433-6c6c-45c5-bb28-d33af72b78b4",
+            doNotHash: false,
+            doNotCopy: false,
+            hashAddressBar: false
+          });
+        </script>
+      </div>
+    </div>
+
+    <script type="text/javascript">
+      function showDesc(){
+         jQuery('#sp-desc').css('height', 'auto');
+         jQuery('#more-desc').hide();
+      }
+    </script>
+  <?php endif; ?>
+
+</div>
+<?php if($page) : ?>
+  <?php // print theme('issue_arguments', $node); ?>
+<?php endif; ?>
+        
