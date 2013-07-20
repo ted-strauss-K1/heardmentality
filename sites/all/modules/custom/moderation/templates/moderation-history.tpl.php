7b450a86ff46825ceac10a65310c2b6b93fbe07e
skullhole <skullhole@gmail.com>
2013-07-20 21:16:11 +0700

    Moderation history

--- sites/all/modules/custom/moderation/templates/moderation-history.tpl.php
+++ sites/all/modules/custom/moderation/templates/moderation-history.tpl.php
@@ -3,46 +3,50 @@
   <?php foreach ($history as $item) : ?>
     <div style="border-bottom: 1px #ccc dotted; margin-bottom: 5px; padding: 5px 0;">
       <?php
-      $account = user_load($item['uid']);
-      $link = l($account->name, $account->viewlink);
+        $account = user_load($item['uid']);
+        $link = l($account->name, $account->viewlink);
       ?>
 
       <?php if ($item['type'] == 'approve') : ?>
-      <?php echo t('Approved by'), ' ', $link; ?>
+
+        <?php echo t('Approved by'), ' ', $link; ?>
+
       <?php elseif ($item['type'] == 'delete') : ?>
-      <?php echo t('Deletion requested by'), ' ', $link; ?>
+
+        <?php echo t('Deletion requested by'), ' ', $link; ?>
+
       <?php elseif ($item['type'] == 'edit') : ?>
-      <?php echo t('Edited by'), ' ', $link; ?>
-      <ul>
-        <?php foreach ($item['vars'] as $main => $edit) : ?>
-        <li>
+
+        <?php echo t('Edited by'), ' ', $link; ?>
+
+        <ul><?php foreach ($item['vars'] as $main => $edit) : ?><li>
+
           <?php if ($main == 'choices') : ?>
-          <?php foreach ($edit as $choice) : ?>
-            <?php if (isset($choice['chtext'])) : ?>
-              <?php if ($choice['chtext'] == '') : ?>
-                <span><?php print t('Requested deletion of choice #') . ($choice['chorder'] + 1); ?></span>
+
+            <?php foreach ($edit as $choice) : ?>
+              <?php if (isset($choice['chtext'])) : ?>
+                <?php if ($choice['chtext'] == '') : ?>
+                  <span><?php print t('Requested deletion of choice #') . ($choice['chorder'] + 1); ?></span>
                 <?php else : ?>
                 <span><?php print t('Changed the text of choice #') . ($choice['chorder'] + 1) . ' ' . t('to') . ' "' . $choice['chtext'] . '"'; ?></span>
                 <?php endif; ?>
               <?php endif; ?>
             <?php if (isset($choice['chtext_short'])) : ?>
               <span><?php print t('Changed the short text of choice #') . ($choice['chorder'] + 1) . ' ' . t('to') . ' "' . $choice['chtext_short'] . '"'; ?></span>
-              <?php endif; ?>
-            <?php endforeach; ?>
-          <?php elseif ($main == 'body') : ?>
-          <?php print t('Set description value to: ') . $edit; ?>
-          <?php elseif ($main == 'title') : ?>
-          <?php print t('Set title to: ') . $edit; ?>
-          <?php elseif ($main == 'comment') : ?>
-          <?php print t('Set text value to: ') . $edit; ?>
-          <?php elseif ($main == 'str_wk') : ?>
-          <?php print t('Set strength value to: ') . t($edit ? 'Strenghten' : 'Weaken'); ?>
+                <?php endif; ?>
+              <?php endforeach; ?>
 
+            <?php elseif ($main == 'body') : ?>
 
-          <?php endif; ?>
-        </li>
-        <?php endforeach; ?>
-      </ul>
+              <?php print t('Set description value to: ') . $edit; ?>
+
+            <?php elseif ($main == 'title') : ?>
+
+              <?php print t('Set title to: ') . $edit; ?>
+
+            <?php endif; ?>
+
+        </li><?php endforeach; ?></ul>
       <?php endif; ?>
     </div>
     <?php endforeach; ?>
