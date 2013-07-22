cf2b7d0651bd0906afd2173848bf7453ce08fa93
skullhole <skullhole@gmail.com>
2013-07-22 11:36:09 +0700

    event following

--- sites/all/modules/custom/events/events_follower/events-follower-content.tpl.php
+++ sites/all/modules/custom/events/events_follower/events-follower-content.tpl.php
@@ -7,20 +7,8 @@
   <!--  | <a name=>References</a>-->
 </div>
 
-
 <hr class="line">
 
 <div id="events_following">
-  <?php
-//  $p = array();
-//  $p['uid'] = events_follower_uids();
-//  $events = events_get($p);
-//  print theme('events', $events);
-  print theme('events', events_get(array(
-    'q' => array(
-      events_build_query(array('uid' => events_follower_uids(),)),
-      events_build_query(array('type' => 'taxonomy',)),
-    )
-  )));
-  ?>
+  <?php print $content ?>
 </div>
