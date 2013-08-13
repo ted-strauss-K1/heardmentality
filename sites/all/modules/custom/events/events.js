28f9ffd31af92ca467341660a54fdf235bd37c6f
skullhole <skullhole@gmail.com>
2013-08-13 15:23:45 +0700

    Translation link fix

--- sites/all/modules/custom/events/events.js
+++ sites/all/modules/custom/events/events.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
   setInterval(function () {
     $('.submitted').each(function() {
       $(this).html(
-        events_time_interval(parseInt($(this).attr('name'))) + " " + events_time_string("ago"));
+        events_time_interval(parseInt($(this).attr('name'))) + " " + Drupal.t("ago"));
     });
   }, 1000);
 });
@@ -34,18 +34,6 @@ function eventsStreamUpdate() {
 }
 
 /**
- * Strings
- *
- * // todo need to have better way to translate JS strings
- *
- * @param string
- * @return {*}
- */
-function events_time_string(string) {
-  return typeof Drupal.settings.time == "undefined" ? string : Drupal.settings.time[string];
-}
-
-/**
  *
  * @param time
  * @return {String}
@@ -56,17 +44,17 @@ function events_time_interval(time) {
 
   var diff = curdate.getTime() - date.getTime();
   if (diff <= 0) {
-    return events_time_string("just now");
+    return Drupal.t("just now");
   }
   diff = Math.floor(diff/1000);
-  if (diff < 60) return diff + " " + events_time_string("second" + (diff==1?"":"s"));
+  if (diff < 60) return diff + " " + Drupal.t("second" + (diff==1?"":"s"));
   diff = Math.floor(diff/60);
-  if (diff < 60) return diff + " " + events_time_string("minute" + (diff==1?"":"s"));
+  if (diff < 60) return diff + " " + Drupal.t("minute" + (diff==1?"":"s"));
   diff = Math.floor(diff/24);
-  if (diff < 24) return diff + " " + events_time_string("hour" + (diff==1?"":"s"));
+  if (diff < 24) return diff + " " + Drupal.t("hour" + (diff==1?"":"s"));
   diff = Math.floor(diff/30);
-  if (diff < 30) return diff + " " + events_time_string("day" + (diff==1?"":"s"));
+  if (diff < 30) return diff + " " + Drupal.t("day" + (diff==1?"":"s"));
   diff = Math.floor(diff/12);
-  if (diff < 12) return diff + " " + events_time_string("month" + (diff==1?"":"s"));
-  return diff + " " + events_time_string("year" + (diff==1?"":"s"));
+  if (diff < 12) return diff + " " + Drupal.t("month" + (diff==1?"":"s"));
+  return diff + " " + Drupal.t("year" + (diff==1?"":"s"));
 }
