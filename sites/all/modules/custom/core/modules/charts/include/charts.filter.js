8dd4cc71ca3ac4d91f9141a5d5ac64ec8f8e74d7
skullhole <skullhole@gmail.com>
2013-09-19 16:29:10 +0700

    Filter chart

--- sites/all/modules/custom/core/modules/charts/include/charts.filter.js
+++ sites/all/modules/custom/core/modules/charts/include/charts.filter.js
@@ -20,188 +20,77 @@ function init_highchart(arg) {
   var js_data = Drupal.settings.charts.filter[arg];
 
   var data = [];
-  for (var i=0; i<Drupal.settings.charts.count; i++) {
+  var max_label_length = 0;
 
+  //
+  if (arg) {
+    for (var i = 0; i < Drupal.settings.charts.count; i++) {
+
+      var data_item = {
+        key   : "Series " + i,
+        color : Drupal.settings.charts.colors[i],
+        values: []
+      };
+
+      for (var j in js_data['#options']) {
+        data_item['values'].push({
+          label: js_data['#options'][j],
+          value: js_data['#results'][i][j]
+        });
+
+        max_label_length = Math.max(max_label_length, (js_data['#options'][j]).length);
+      }
+
+      data.push(data_item);
+    }
+
+    console.log(JSON.stringify(data));
+  }
+
+  //
+  else {
     var data_item = {
-      key : i,
-      color : Drupal.settings.charts.colors[i],
+      key   : 0,
+      // color : Drupal.settings.charts.colors[0],
       values: []
     };
 
-    for (var j in js_data['#options']) {
+    for (var i = 0; i < Drupal.settings.charts.count; i++) {
       data_item['values'].push({
-        label : js_data['#options'][j],
-        value : js_data['#results'][i][j]
+        label: js_data['#options'][i],
+        value: js_data['#results'][i][i],
+        color: Drupal.settings.charts.colors[i]
       });
+
+      max_label_length = Math.max(max_label_length, (js_data['#options'][i]).length);
     }
 
     data.push(data_item);
   }
 
-  //
-  console.log(js_data);
-  console.log(data);
+  $('#chart-filter svg').empty();
 
   var chart;
-  nv.addGraph(function() {
+  nv.addGraph(function () {
     chart = nv.models.multiBarHorizontalChart()
-      .x(function(d) { return d.label })
-      .y(function(d) { return d.value })
-      .margin({top: 0, right: 20, bottom: 20, left: 80})
-      .transitionDuration(250)
-      .showControls(false)
-      .showLegend(false);
-      // .staggerLabels(true);
+      .x(function (d) {
+        return d.label
+      })
+      .y(function (d) {
+        return d.value
+      })
+      .margin({top: 0, right: 20, bottom: 20, left: 20 + 5 * max_label_length})
+      .showLegend(false)
+      .showControls(false);
+
+    if (!arg) chart.barColor(d3.scale.category20().range());
 
     d3.select('#chart-filter svg')
       .datum(data)
       .call(chart);
-
-    var insertLinebreaks = function (d) {
-      var el = d3.select(this);
-      var words = d.split(' ');
-      el.text('');
-
-      for (var i = 0; i < words.length; i++) {
-        var tspan = el.append('tspan').text(words[i]);
-        if (i > 0)
-          tspan.attr('x', 0).attr('dy', '15');
-      }
-    };
-
-    d3.selectAll('g.x.axis g text').each(insertLinebreaks);
 
     nv.utils.windowResize(chart.update);
 
     return chart;
   });
 }
-
-long_short_data = [
-  {
-    key: 'Series1',
-    color: '#d62728',
-    values: [
-      {
-        "label" : "Group A" ,
-        "value" : -1.8746444827653
-      } ,
-      {
-        "label" : "Group B" ,
-        "value" : -8.0961543492239
-      } ,
-      {
-        "label" : "Group C" ,
-        "value" : -0.57072943117674
-      } ,
-      {
-        "label" : "Group D" ,
-        "value" : -2.4174010336624
-      } ,
-      {
-        "label" : "Group E" ,
-        "value" : -0.72009071426284
-      } ,
-      {
-        "label" : "Group F" ,
-        "value" : -0.77154485523777
-      } ,
-      {
-        "label" : "Group G" ,
-        "value" : -0.90152097798131
-      } ,
-      {
-        "label" : "Group H" ,
-        "value" : -0.91445417330854
-      } ,
-      {
-        "label" : "Group I" ,
-        "value" : -0.055746319141851
-      }
-    ]
-  },
-  {
-    key: 'Series2',
-    color: '#1f77b4',
-    values: [
-      {
-        "label" : "Group A" ,
-        "value" : 25.307646510375
-      } ,
-      {
-        "label" : "Group B" ,
-        "value" : 16.756779544553
-      } ,
-      {
-        "label" : "Group C" ,
-        "value" : 18.451534877007
-      } ,
-      {
-        "label" : "Group D" ,
-        "value" : 8.6142352811805
-      } ,
-      {
-        "label" : "Group E" ,
-        "value" : 7.8082472075876
-      } ,
-      {
-        "label" : "Group F" ,
-        "value" : 5.259101026956
-      } ,
-      {
-        "label" : "Group G" ,
-        "value" : 0.30947953487127
-      } ,
-      {
-        "label" : "Group H" ,
-        "value" : 0
-      } ,
-      {
-        "label" : "Group I" ,
-        "value" : 0
-      }
-    ]
-  },
-  {
-    key: 'Series3',
-    color: '#2ca02c',
-    values: [
-      {
-        "label" : "Group A" ,
-        "value" : -25.307646510375
-      } ,
-      {
-        "label" : "Group B" ,
-        "value" : 16.756779544553
-      } ,
-      {
-        "label" : "Group C" ,
-        "value" : -18.451534877007
-      } ,
-      {
-        "label" : "Group D" ,
-        "value" : 8.6142352811805
-      } ,
-      {
-        "label" : "Group E" ,
-        "value" : -7.8082472075876
-      } ,
-      {
-        "label" : "Group F" ,
-        "value" : 5.259101026956
-      } ,
-      {
-        "label" : "Group G" ,
-        "value" : -0.30947953487127
-      } ,
-      {
-        "label" : "Group H" ,
-        "value" : 0
-      } ,
-      {
-        "label" : "Group I" ,
-        "value" : 0
-      }
-    ]
-  }
-];
