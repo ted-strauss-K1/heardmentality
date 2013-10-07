d6919cec0010a44244433a83bc692a36c81e343c
skullhole <skullhole@gmail.com>
2013-10-07 13:42:05 +0700

    Reformat code

--- embed.js
+++ embed.js
@@ -1,5 +1,4 @@
-(function(d){
-
+(function (d) {
   var id = 'hm-embed', a = "data-issue", l = 'http://dev.heardmentality.org', w, i;
   if ((w = document.getElementById(id)) && (i = w.getAttribute(a))) {
 
@@ -8,7 +7,7 @@
     js = d.createElement('script');
     js.async = true;
     js.src = l + '/sites/all/libraries/easyxdm/easyXDM.debug.js';
-    js.onreadystatechange= function () {
+    js.onreadystatechange = function () {
       if (this.readyState == 'complete') embed();
     }
     js.onload = embed;
@@ -17,9 +16,9 @@
     // actual embed using easyxdm
     function embed() {
       new easyXDM.Socket({
-        remote: l + "/embedder/" + i,
+        remote   : l + "/embedder/" + i,
         container: w,
-        onMessage: function(message, origin){
+        onMessage: function (message, origin) {
           this.container.getElementsByTagName("iframe")[0].style.height = message + "px";
         }
       });
