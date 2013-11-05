e15c9d997d59a1069a5cb5740f749165b4acf1ed
skullhole <skullhole@gmail.com>
2013-11-05 16:28:54 +0700

    Points tracking

---
+++ sites/all/modules/custom/game/points/points.track.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  stLight.subscribe("click", points_share);
+});
+
+/**
+ *
+ * @param event
+ * @param service
+ */
+function points_share(event, service) {
+  $.ajax({
+    type    : 'POST',
+    dataType: 'json',
+    url     : Drupal.settings.language_prefix + '/action/queue',
+    data    : {
+      'uid'       : Drupal.settings.user.uid,
+      'operation' : 'share',
+      'service'   : service,
+      'entity_id' : Drupal.settings.node.nid,
+      'entity_nid': 'node'
+    },
+    success : function (response) {
+      if (!response.status) {
+        // on failure
+        return false;
+      }
+      // on success
+    }
+  });
+}
