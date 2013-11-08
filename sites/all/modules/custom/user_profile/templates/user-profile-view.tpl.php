d7fbb89c105da776593d7a62478937b8beb605a8
skullhole <skullhole@gmail.com>
2013-11-08 15:31:21 +0700

    Login redirect

--- sites/all/modules/custom/user_profile/templates/user-profile-view.tpl.php
+++ sites/all/modules/custom/user_profile/templates/user-profile-view.tpl.php
@@ -3,11 +3,11 @@ print theme('follower_button', $account);
 
 global $user;
 if ($user->uid == $account->uid) {
-  print l(t('Edit Profile'), 'user/profile/edit',
-    array('attributes' => array(
-      'class' => 'button light editlink',
-    ))
-  );
+  print l(t('Edit Profile'), 'user/profile/edit', array(
+      'attributes' => array(
+        'class' => 'button light editlink',
+      )
+    ));
 }
 ?>
 <h2 class="din"><?php print $account->name; ?></h2>
@@ -21,30 +21,30 @@ if ($user->uid == $account->uid) {
       <span class="URL block">
           <strong><?php print t($field->title); ?>:</strong>
         <?php switch ($field->name) :
-        case 'profile_twitter' :
-          ?>
-          <a href="https://twitter.com/<?php print htmlspecialchars($account->{$field->name}); ?>"
-             target="_blank"><?php print $account->{$field->name}; ?></a>
-          <?php break;
-        case 'profile_facebook':
-          ?>
-          <a href="http://www.facebook.com/<?php print htmlspecialchars($account->{$field->name}); ?>"
-             target="_blank"><?php print $account->{$field->name}; ?></a>
-          <?php break;
-        case 'profile_website':
-          ?>
-          <a href="<?php print htmlspecialchars($account->{$field->name}); ?>"
-             target="_blank"><?php print $account->{$field->name}; ?></a>
-          <?php break;
+          case 'profile_twitter' :
+            ?>
+            <a href="https://twitter.com/<?php print htmlspecialchars($account->{$field->name}); ?>"
+               target="_blank"><?php print $account->{$field->name}; ?></a>
+            <?php break;
+          case 'profile_facebook':
+            ?>
+            <a href="http://www.facebook.com/<?php print htmlspecialchars($account->{$field->name}); ?>"
+               target="_blank"><?php print $account->{$field->name}; ?></a>
+            <?php break;
+          case 'profile_website':
+            ?>
+            <a href="<?php print htmlspecialchars($account->{$field->name}); ?>"
+               target="_blank"><?php print $account->{$field->name}; ?></a>
+            <?php break;
 
-        default :
-          ?>
+          default :
+            ?>
             <?php print $account->{$field->name}; ?>
             <?php break; ?>
             <?php endswitch; ?>
         </span>
-      <?php endif; ?>
-    <?php endforeach; ?>
+    <?php endif; ?>
+  <?php endforeach; ?>
 
 </div>
 
