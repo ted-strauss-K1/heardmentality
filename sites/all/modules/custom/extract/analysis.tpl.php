2d63babfac2e619b9259cdc3ba09840791d9b7f3
skullhole <skullhole@gmail.com>
2013-11-12 17:38:05 +0700

    Analysis Page

---
+++ sites/all/modules/custom/extract/analysis.tpl.php
@@ -0,0 +1,26 @@
+<script type="text/javascript" src="http://public.tableausoftware.com/javascripts/api/viz_v1.js"></script>
+<div class="tableauPlaceholder" style="width:100%; height:657px;">
+  <noscript><a href="#"><img alt="Demographics "
+                             src="http:&#47;&#47;public.tableausoftware.com&#47;static&#47;images&#47;An&#47;Analysis_5&#47;Users&#47;1_rss.png"
+                             style="border: none"/></a></noscript>
+  <object class="tableauViz" width="100%" height="657" style="display:none;">
+    <param name="host_url" value="http%3A%2F%2Fpublic.tableausoftware.com%2F"/>
+    <param name="site_root" value=""/>
+    <param name="name" value="Analysis_5&#47;Users"/>
+    <param name="tabs" value="no"/>
+    <param name="toolbar" value="yes"/>
+    <param name="static_image"
+           value="http:&#47;&#47;public.tableausoftware.com&#47;static&#47;images&#47;An&#47;Analysis_5&#47;Users&#47;1.png"/>
+    <param name="animate_transition" value="yes"/>
+    <param name="display_static_image" value="yes"/>
+    <param name="display_spinner" value="yes"/>
+    <param name="display_overlay" value="yes"/>
+    <param name="display_count" value="yes"/>
+  </object>
+</div>
+<div
+  style="width:100%;height:22px;padding:0px 10px 0px 0px;color:black;font:normal 8pt verdana,helvetica,arial,sans-serif;">
+  <div style="float:right; padding-right:8px;"><a
+      href="http://www.tableausoftware.com/public?ref=http://public.tableausoftware.com/views/Analysis_5/Users"
+      target="_blank">Powered by Tableau</a></div>
+</div>
