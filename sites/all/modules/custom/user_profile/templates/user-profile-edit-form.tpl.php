d36d588498f10280b2a22c4bb042627c4bb8e437
skullhole <dyachenko.oleg@gmail.com>
2014-01-27 16:30:45 +0700

    No block on profile edit

--- sites/all/modules/custom/user_profile/templates/user-profile-edit-form.tpl.php
+++ sites/all/modules/custom/user_profile/templates/user-profile-edit-form.tpl.php
@@ -48,26 +48,22 @@ $privacy = user_profile_profile_privacy_values();
     </div>
   </div>
 
-  <!--  <div class="grey-box full clearfix">--><!--    <label for="" class="subscriptions">-->
-  <?php //print t('Social Integration'); ?><!--</label>--><!----><!--    <p class="twelve">-->
-  <!--      --><?php //print t('Stay logged in to these accounts - This makes sharing with your external networks easier.'); ?>
-  <!--    </p>--><!--    --><?php //print drupal_render($form['social']); ?>
-  <!--  </div>-->
+  <?php if ($form['subscription_notify']) : ?>
+    <div class="grey-box full clearfix">
+      <label for="" class="subscriptions"><?php print t('Subscriptions'); ?></label>
 
-  <div class="grey-box full clearfix">
-    <label for="" class="subscriptions"><?php print t('Subscriptions'); ?></label>
+      <p class="twelve"><?php print t('Send me a summary of what\'s happening on Heard Mentality to my email'); ?></p>
+      <ul class="subscription">
+        <li><span class="privacy-answer"><?php print t('Never'); ?></span><br></li>
+        <li><span class="privacy-answer"><?php print t('Monthly'); ?></span><br></li>
+        <li><span class="privacy-answer"><?php print t('Weekly'); ?></span></li>
 
-    <p class="twelve"><?php print t('Send me a summary of what\'s happening on Heard Mentality to my email'); ?></p>
-    <ul class="subscription">
-      <li><span class="privacy-answer"><?php print t('Never'); ?></span><br></li>
-      <li><span class="privacy-answer"><?php print t('Monthly'); ?></span><br></li>
-      <li><span class="privacy-answer"><?php print t('Weekly'); ?></span></li>
-
-      <?php print drupal_render($form['subscription_notify']); ?>
-      <?php print drupal_render($form['subscription_email']); ?>
-      <?php print drupal_render($form['subscription_submit']); ?>
-    </ul>
-  </div>
+        <?php print drupal_render($form['subscription_notify']); ?>
+        <?php print drupal_render($form['subscription_email']); ?>
+        <?php print drupal_render($form['subscription_submit']); ?>
+      </ul>
+    </div>
+  <?php endif; ?>
 
   <div class="grey-box full clearfix">
     <label for="" class="subscriptions"><?php print t('Language settings'); ?></label>
