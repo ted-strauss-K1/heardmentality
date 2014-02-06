1511fb2fb06996d1374734b69642b49cd6087da4
skullhole <dyachenko.oleg@gmail.com>
2014-02-06 15:13:52 +0700

    history chart change

--- sites/all/modules/custom/core/modules/charts/include/charts.history.js
+++ sites/all/modules/custom/core/modules/charts/include/charts.history.js
@@ -9,45 +9,103 @@ $(function () {
  *
  */
 function init_history() {
+  // get data
+  var charts = Drupal.settings.charts, history = charts.history;
+
   nv.addGraph(function () {
-    var chart = nv.models.lineWithFocusChart();
+    // +focus
+    // var chart = nv.models.lineWithFocusChart();
+    var chart = nv.models.lineChart();
 
     // settings
     chart
       .interpolate("basis")
       .margin({left: 20, right: 20, top: 10})
-      .height2(40)
+      // +focus
+      // .height2(40)
       .showLegend(false);
     // axis
     chart.xAxis.tickFormat(timestamp2date);
-    chart.x2Axis.tickFormat(timestamp2date);
-    // chart.xAxis.rotateLabels(-45);
+    // +focus
+    // chart.x2Axis.tickFormat(timestamp2date);
+
+    // dates
+    var dates = [], days = 0;
+    for (dates[days++] in history['stats']);
 
     // get data
-    var count = Drupal.settings.charts.count;
-    var dates = [], i = 0;
-    for (dates[i++] in Drupal.settings.charts.history['stats']) {
-      //
-    }
-    var datum = [];
-    var max = 0;
+    var count = charts.count;
+
+    // datum
+//    var datum = [];
+//    var max = 0;
+//    for (var i = 0; i < count; i++) {
+//      var series = {};
+//      series['color'] = charts.colors[i];
+//      var choice_data = history['choice' + i];
+//      series['key'] = choice_data['name'];
+//      series['values'] = [];
+//      for (var j in choice_data['data']) {
+//        series['values'].push({
+//          series: i,
+//          x     : new Date(dates[j]),
+//          y     : choice_data['data'][j]
+//        });
+//      }
+//
+//      max = Math.max(max, Math.max.apply(null, choice_data['data']));
+//
+//      datum[i] = series;
+//    }
+
+
+    // datum 2
+    var datum = [], max = 0;
+    // save key names
     for (var i = 0; i < count; i++) {
-      var series = {};
-      series['color'] = Drupal.settings.charts.colors[i];
-      var choice_data = Drupal.settings.charts.history['choice' + i];
-      series['key'] = choice_data['name'];
-      series['values'] = [];
-      for (var j in choice_data['data']) {
-        series['values'].push({
-          series: i,
-          x     : new Date(dates[j]),
-          y     : choice_data['data'][j]
-        });
+      datum.push({
+        color : charts.colors[i],
+        key   : history['choice' + i]['name'],
+        values: []
+      });
+    }
+    // add data
+    var data_prev = [], data_upd = false, data_temp = [];
+    for (var j = 0; j < days; j++) {
+      var date = new Date(dates[j]), choice;
+
+      //
+      data_temp = [];
+      for (var i = 0; i < count; i++) {
+        var votes = history['choice' + i]['data'][j];
+        data_temp.push(votes);
+        max = Math.max(max, votes);
       }
 
-      max = Math.max(max, Math.max.apply(null, choice_data['data']));
+      //
+      if (0 == j) {
+        data_prev = data_temp;
+      }
 
-      datum[i] = series;
+      //
+      data_upd = false;
+      for (var k = 0; k < data_temp.length; k++) {
+        if (data_prev[k] != data_temp[k]) {
+          data_upd = true;
+          data_prev = data_temp;
+          break;
+        }
+      }
+
+      if (data_upd) {
+        for (var i = 0; i < count; i++) {
+          (datum[i]['values']).push({
+            series: i,
+            x     : date,
+            y     : history['choice' + i]['data'][j]
+          });
+        }
+      }
     }
 
     chart.forceY([0, max + 1]);
