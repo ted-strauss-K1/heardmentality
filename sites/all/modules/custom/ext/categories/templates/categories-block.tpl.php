9fb81706a3aad2f8958fc8076c51239a145e36af
skullhole <dyachenko.oleg@gmail.com>
2014-04-24 16:27:37 +0700

    translations

--- sites/all/modules/custom/ext/categories/templates/categories-block.tpl.php
+++ sites/all/modules/custom/ext/categories/templates/categories-block.tpl.php
@@ -2,7 +2,7 @@
   <div class="grey-box poll-box">
     <i class="icon cat text icon-tag"></i>
     <ul class="tags">
-      <label class="tags-on"><?php print t('This issue is listed under'); ?></label>
+      <label class="tags-on"><?php print __('This issue is listed under', array('@code' => 'categories-listed')); ?></label>
       <?php if ($categories) {
         foreach ($categories as $hierarchy => $name) : ?>
           <li><?php print theme('categories_subscribe', $name, $hierarchy); ?></li>
@@ -15,18 +15,27 @@
 
     <p class="issue-meta">
       <?php if ($author_status) : ?>
-        <?php print t('Posted by'); ?>
-        <?php print l($node->name, 'user/profile/view/' . $node->uid, array('attributes' => array('class' => 'username'))); ?>
-        <?php print t('on') ?>
+        <?php print __('Posted by !name on !time', array(
+          '@code' => 'categories-posted-by-on',
+          '!name' => l($node->name, 'user/profile/view/' . $node->uid, array('attributes' => array('class' => 'username'))),
+          '!time' => date('M j, Y @ H:i', $node->created),
+        ));?>
       <?php else : ?>
-        <?php print t('Posted on'); ?>
+        <?php print __('Posted on !time', array(
+          '@code' => 'categories-posted-on',
+          '!time' => date('M j, Y @ H:i', $node->created),
+        )); ?>
       <?php endif; ?>
       <br>
-      <?php print date('M j, Y @ H:i', $node->created); ?>.
       <?php if ($original) : ?>
         <br>
-        <?php print t('Read original post in'); ?>
-        <?php print l($original['language'], $original['path']); ?>
+        <?php
+          $original['language'] = __($original['language'], array('@code' => 'language-' . $original['langcode']));
+        ?>
+        <?php print __('Read original post in !language', array(
+          '@code' => 'categories-read-original',
+          '!language' => l($original['language'], $original['path']),
+        )); ?>
       <?php endif; ?>
     </p>
 
--- sites/all/modules/custom/ext/categories/templates/categories-user-block.tpl.php
+++ sites/all/modules/custom/ext/categories/templates/categories-user-block.tpl.php
@@ -2,7 +2,7 @@
   <div class="grey-box poll-box">
     <i class="icon cat text icon-tag"></i>
     <ul class="tags">
-      <label class="tags-on"><?php print t('SUBSCRIPTIONS'); ?></label>
+      <label class="tags-on"><?php print __('SUBSCRIPTIONS', array('@code' => 'categories-subscriptions')); ?></label>
       <?php foreach ($categories as $term) : ?>
         <li><?php print theme('categories_subscribe', $term->name, term_hierarchy($term)); ?></li>
       <?php endforeach; ?>
