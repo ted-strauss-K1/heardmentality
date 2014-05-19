300fdb2a95315c984f6afc18c3117877f0844e9a
Elizaveta Kabanova <iiiameye@gmail.com>
2014-05-19 16:08:59 +0700

    newsletters - tables in tables

--- sites/all/themes/heardmentalitylight/newsletter-content.tpl.php
+++ sites/all/themes/heardmentalitylight/newsletter-content.tpl.php
@@ -1,61 +1,83 @@
 <?php global $theme_path; ?>
 
 <?php foreach ($data['nids'] as $nid) :
-$index++; ?>
-<?php
-$uid = 1;
+    $index++; ?>
+    <?php
+    $uid = 1;
 
-$i = -1;
-$choices = cpoll_load_choices($nid);
-$choices_count = count($choices);
+    $i = -1;
+    $choices = cpoll_load_choices($nid);
+    $choices_count = count($choices);
 
-$node = node_load($nid);
-$node->debate = argument_get_list($node->nid, $node->info);
-$account = user_load($node->uid);
+    $node = node_load($nid);
+    $node->debate = argument_get_list($node->nid, $node->info);
+    $account = user_load($node->uid);
 
-?>
+    ?>
 
-<table style="border-spacing: 0; width: 100%">
-    <tbody>
-    <tr class="tr_outer">
-        <td class="td_with_cross_texture"
-            style="background: url(<?php print url($theme_path . '/images/cross_hatch_texture.png', array('absolute' => TRUE)); ?>) ;
-                border-left: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0; font-family: Arial; padding: 12px;">
+    <table style="border-spacing: 0; width: 100%">
+        <tbody>
+        <tr>
+            <td style="background: #e2e2e2 ;
+                border-left: 1px solid #c0c0c0;
+                border-right: 1px solid #c0c0c0;
+                font-family: Arial;
+                padding: 12px;">
 
+                <table width="100%"
+                       style="border-spacing: 0;
+                       background: #ffffff;
+                   border-top: 1px solid #c0c0c0;
+                   border-left: 1px solid #c0c0c0;
+                   border-right: 1px solid #c0c0c0;
+                   margin: 15px 0 0 0;
+                   padding: 20px 12px 10px 12px;">
+                    <tbody>
+                    <tr>
+                        <td>
+                            <div style="background: #ffffff;
+                        padding: 0 0 5px 0;
+                        font-size: 20px"><?php
+                                print l($node->title, 'node/' . $nid, array(
+                                    'absolute' => TRUE,
+                                    'attributes' => array(
+                                        'style' => 'text-decoration: none; color: #3c3c3c; font-size: 20px;',
+                                    ),
+                                ));
+                                ?>?
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td rowspan="1" colspan="1"
+                            style=" width: 100%;
+                        font-family: Arial;
+                        padding: 2px 8px;
+                        font-size: 10px;
+                        text-align: right;
+                        background: #eee;
+                        font-style: italic;
+                        background: #76d2dc;
+                        border: 1px solid #92b7dd;
+                        color: #362750;">
+                            <?php print intval($node->info['argument']); ?>&nbsp;<?php print t('ARGUMENTS'); ?>
+                            &nbsp;|&nbsp;
+                            <?php print intval($node->info['resource']); ?>&nbsp;<?php print t('REFERENCES'); ?>
+                            &nbsp;|&nbsp;
+                            <?php print intval($node->votecount); ?>&nbsp;<?php print t('VOTES'); ?>
 
-
-
-            <table width="100%" class="table_title_of_issure"
-                   style="border-spacing: 0; width=100%; background: #ffffff; border-top: 1px solid #c0c0c0; border-left: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0; margin: 28px 0 0 0; padding: 20px 12px 8px 12px;">
-                <tbody>
-                <tr>
-                    <td rowspan="1" colspan="1" style="padding: 0; margin: 0;">
-                        <div style="background: #ffffff; padding: 0 0 5px 0;"><?php
-                            print l($node->title, 'node/' . $nid, array(
-                                'absolute' => TRUE,
-                                'attributes' => array(
-                                    'style' => 'text-decoration: none; color: #3c3c3c; font-size: 20px;',
-                                ),
-                            ));
-                            ?>?
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td rowspan="1" colspan="1"
-                        style=" width: 100%; font-family: Arial; padding: 2px 8px; font-size: 10px; text-align: right;
-                            background: #eee; font-style: italic; background: #76d2dc; border: 1px solid #92b7dd; color: #362750;">
-                        <?php print intval($node->info['argument']); ?>&nbsp;<?php print t('ARGUMENTS'); ?>&nbsp;|&nbsp;
-                        <?php print intval($node->info['resource']); ?>&nbsp;<?php print t('REFERENCES'); ?>&nbsp;|&nbsp;
-                        <?php print intval($node->votecount); ?>&nbsp;<?php print t('VOTES'); ?>
-
-                    </td>
-                </tr>
-                </tbody>
-            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
 
                 <table class="variant_answer"
-                       style="border-spacing: 0; width: 100%; background: #ffffff; border-left: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0; padding: 0 12px;">
+                       style="border-spacing: 0;
+                       width: 100%;
+                       background: #ffffff;
+                       border-left: 1px solid #c0c0c0;
+                       border-right: 1px solid #c0c0c0;
+                       padding: 0 12px;">
                     <tbody>
                     <?php foreach ($choices as $chid => $choice) :
                         $i++;
@@ -63,12 +85,25 @@ $account = user_load($node->uid);
                         <tr>
                             <?php if (is_numeric($chid)) : ?>
                                 <td rowspan="1" colspan="1" nowrap="nowrap" width="60px"
-                                    style="height: 44px; padding: 0;">
+                                    style="height: 44px;
+                                    padding: 0;">
                                     <a
                                         href="<?php print $uid ? cpoll_vote_external_link($uid, $chid, TRUE) : url('node/' . $nid, array('absolute' => TRUE)) ?>"
-                                        style=" background: none #4298EF; border: 1px solid #849EB7; padding: 12px 6px; margin-right: 10px; color: #fff; font-weight: bold; font-size: 15px; text-decoration: none; letter-spacing: 1px; height: 44px;">VOTE</a>
+                                        style=" background: none #4298EF;
+                                        border: 1px solid #849EB7;
+                                        padding: 12px 6px;
+                                        margin-right: 10px;
+                                        color: #fff;
+                                        font-weight: bold;
+                                        font-size: 15px;
+                                        text-decoration: none;
+                                        letter-spacing: 1px;
+                                        height: 44px;">VOTE</a>
                                 </td>
-                                <td style="background: #e4e4e4; padding: 0 10px; height: 42px; border: 1px solid #d7d7d7;">
+                                <td style="background: #e4e4e4;
+                                padding: 0 10px;
+                                height: 42px;
+                                border: 1px solid #d7d7d7;">
                                     <span><?php print $choice['chtext']; ?></span>
                                 </td>
                             <?php else: ?>
@@ -76,7 +111,8 @@ $account = user_load($node->uid);
                                 </td>
                             <?php endif; ?>
                         </tr>
-                        <tr style="width: 100%; height: 6px;"></tr>
+                        <tr style="width: 100%;
+                        height: 8px;"></tr>
                     <?php endforeach; ?>
 
 
@@ -84,17 +120,27 @@ $account = user_load($node->uid);
                 </table>
 
                 <table
-                    style="border-spacing: 0; width: 100%; height: 22px; background: #ffffff; border-bottom: 1px solid #c0c0c0; border-left: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0;">
-                    <tbody></tbody>
+                    style="border-spacing: 0;
+                    width: 100%;
+                    height: 22px;
+                    background: #ffffff;
+                    border-bottom: 1px solid #c0c0c0;
+                    border-left: 1px solid #c0c0c0;
+                    border-right: 1px solid #c0c0c0;">
+                    <tbody>
+                    <tr>
+                        <td style="height: 22px">
+
+                        </td>
+                    </tr>
+                    </tbody>
                 </table>
 
 
+            </td>
+        </tr>
 
-
-                </td>
-                </tr>
-
-    </tbody>
-</table>
+        </tbody>
+    </table>
 <?php endforeach; ?>
 
